a4020bc94f8e19e4ce665ae72eeed57b180c4ecd
Cali0707 <calumramurray@gmail.com>
2022-05-08 14:24:39 -0400

    Added text tool tip

---
+++ client/src/components/text/Text.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+export function TextToolTip({ description, children, width, placement }) {
+  if (description === '' || description === undefined) {
+    return children;
+  } else if (width === undefined) {
+    return (
+      <OverlayTrigger
+        placement={placement ?? 'bottom'}
+        overlay={
+          <Tooltip id={'tooltip'}>
+            <div dangerouslySetInnerHTML={{ __html: description }}></div>
+          </Tooltip>
+        }
+      >
+        {children}
+      </OverlayTrigger>
+    );
+  } else {
+    const length = width ?? '20px';
+    return (
+      <OverlayTrigger
+        placement={this.props.placement === undefined ? 'bottom' : this.props.placement}
+        overlay={
+          <Tooltip id="tooltip">
+            <div dangerouslySetInnerHTML={{ __html: this.props.description }}></div>
+          </Tooltip>
+        }
+      >
+        <img
+          alt="descriptions"
+          src={require('../../../assets/icons/info-circle-solid.svg').default}
+          style={{ width: length, height: length, opacity: 0.8 }}
+        />
+      </OverlayTrigger>
+    );
+  }
+}
+
+TextToolTip.propTypes = {
+  description: PropTypes.string || PropTypes.node || undefined,
+  children: PropTypes.node,
+  width: PropTypes.string,
+  placement: PropTypes.string,
+};
