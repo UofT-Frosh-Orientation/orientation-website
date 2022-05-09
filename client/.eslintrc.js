24d26e78d76768fd383970a518a74d230ffebb38
Cali0707 <calumramurray@gmail.com>
2022-05-08 22:41:32 -0400

    Refactored text components

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
@@ -27,7 +26,6 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
     'no-unused-vars': 'off',
     'no-prototype-builtins': 'off',
   },
---
+++ client/src/components/text/Header/Header.stories.jsx
@@ -0,0 +1,6 @@
+import { Header } from './Header';
+
+export const HeaderSection = () => <Header type={'section'}>Header Section</Header>;
+export const HeaderParagraph = () => <Header type={'paragraph'}>Header Paragraph</Header>;
+export const HeaderBold = () => <Header type={'bold'}>Header Bold</Header>;
+export const HeaderFancy = () => <Header type={'fancy'}>Header Fancy</Header>;
