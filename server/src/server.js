995ae099cc41165d88dd2ccd64de65ead23fcc1f
Cali0707 <calumramurray@gmail.com>
2022-05-23 13:49:35 -0400

    Setup swagger js docs, as well as swagger UI with basic auth

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoLoader = require('./loaders/mongoLoader');
+const swaggerLoader = require('./loaders/swaggerLoader');
 
 const froshRouter = require('./routes/froshRoutes');
 
@@ -8,11 +9,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/frosh', froshRouter);
+
+swaggerLoader(app);
+
 app.get('*', (req, res) => {
   res.status(200).send('Hello, Luke!');
 });
-
-app.use('/frosh', froshRouter);
 
 mongoLoader().then(() => {
   app.listen(process.env.PORT || 5001, () => {
