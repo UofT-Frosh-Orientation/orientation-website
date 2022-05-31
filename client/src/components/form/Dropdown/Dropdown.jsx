e58f8ca96684e6190c1dae465bd35d20d9c13971
Cali0707 <calumramurray@gmail.com>
2022-05-31 09:13:49 -0400

    Added animations, hook to close dropdown on click outside of it

--- client/src/components/form/Dropdown/Dropdown.jsx
+++ client/src/components/form/Dropdown/Dropdown.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import './Dropdown.scss';
 
+const useWrapperRef = (onClickOutside) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClickOutside(event);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+  }, [ref]);
+  return ref;
+};
+
 const Dropdown = ({ items, onSelect, label, selected, isDisabled }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const wrapperRef = useWrapperRef(() => setIsOpen(false));
 
   const dropdownItems = items.map((item, index) => (
     <div
@@ -20,18 +35,15 @@ const Dropdown = ({ items, onSelect, label, selected, isDisabled }) => {
   ));
 
   return (
-    <div className={'dropdown-container'}>
+    <div className={'dropdown-container'} ref={wrapperRef}>
       <div className={'dropdown-header'}>{label}</div>
       <div
         onClick={() => !isDisabled && setIsOpen(!isOpen)}
         className={`dropdown-selected${isDisabled ? '-disabled' : ''}`}
       >
         <div className={'dropdown-selected-label'}>{selected.label}</div>
-        <div className={'dropdown-image'}>
-          <img
-            alt={'arrow'}
-            src={`../../../assets/icons/${isOpen ? 'angle-up-solid.svg' : 'angle-down-solid.svg'}`}
-          />
+        <div className={`dropdown-image${isOpen ? ' open' : ''}`}>
+          <img alt={'arrow'} src={'../../../assets/icons/angle-down-solid.svg'} />
         </div>
       </div>
       {isOpen && <div className={'dropdown-list-container'}>{dropdownItems}</div>}
