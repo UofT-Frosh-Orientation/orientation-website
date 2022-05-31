1b109daa262f216f2555e0e5acf0054bfb7d4761
NeoLou <neolou2000@hotmail.com>
2022-05-31 14:16:37 -0400

    init commit of loading animation

---
+++ client/src/components/misc/LoadingAnimation/LoadingAnimation.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { LoadingAnimation } from './LoadingAnimation';
+
+export default {
+  title: 'LoadingAnimation',
+  component: LoadingAnimation,
+};
+
+export const LoadingAnimationStory = (args) => <LoadingAnimation {...args} />;
+LoadingAnimationStory.storyName = 'LoadingAnimation';
+LoadingAnimationStory.args = {
+  animate: true,
+};
