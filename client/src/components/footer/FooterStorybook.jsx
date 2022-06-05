aa8862ea88be73bb832c348216d5f4076d99cef8
Natalie Chan <natapoke2@gmail.com>
2022-06-05 10:32:08 -0400

    removing extra imports

--- client/src/components/footer/FooterStorybook.jsx
+++ client/src/components/footer/FooterStorybook.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Footer.scss';
-
-import FacebookIcon from '../../assets/social/facebook-square-brands.svg';
-import InstagramIcon from '../../assets/social/instagram-square-brands.svg';
-import TwitterIcon from '../../assets/social/twitter-square-brands.svg';
-import SnapchatIcon from '../../assets/social/snapchat-square-brands.svg';
-import YoutubeIcon from '../../assets/social/youtube-square-brands.svg';
+import { socials } from '../../util/socials';
 
 const pages = {
   main: [
@@ -24,33 +19,6 @@ const pages = {
     },
   ],
 };
-const socials = [
-  {
-    label: 'Facebook',
-    link: 'https://bit.ly/froshfb',
-    icon: FacebookIcon,
-  },
-  {
-    label: 'Instagram',
-    link: 'https://bit.ly/froshig',
-    icon: InstagramIcon,
-  },
-  {
-    label: 'Twitter',
-    link: 'https://bit.ly/froshweektwitter',
-    icon: TwitterIcon,
-  },
-  {
-    label: 'Snapchat',
-    link: 'https://bit.ly/2N3dlXt',
-    icon: SnapchatIcon,
-  },
-  {
-    label: 'YoutubeIcon',
-    link: 'https://bit.ly/froshtube',
-    icon: YoutubeIcon,
-  },
-];
 
 const FooterStorybook = () => {
   return (
