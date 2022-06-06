8dd746315e7d0030d365587c25c46d59d06ef38b
NeoLou <neolou2000@hotmail.com>
2022-06-06 15:28:06 -0400

    animation with css only
    no need for external libraries anymore

--- client/src/components/misc/LoadingAnimation/LoadingAnimation.jsx
+++ client/src/components/misc/LoadingAnimation/LoadingAnimation.jsx
@@ -1,25 +1,20 @@
-// Importing React, css, frosh logo, and spinner animation
-import React, {Component} from 'react';
+// Importing React, css, frosh logo
+import React, { Component } from 'react';
 import './LoadingAnimation.scss';
 import logo from '../../../assets/icons/dino_test.svg'; // Change later: dino_test.svg is just placeholder for now
-import Spinner from 'react-bootstrap/Spinner'; 
 
 // Loading Animation class
 // Note: If we don't want logo spinning, remove animation in css
 class LoadingAnimation extends Component {
-  render(){
+  render() {
     return (
       <div className="loadingAnimationContainer">
-        <div className="loadingAnimation">
-          <Spinner animation="border" role="status"/>
-          <img src={logo} className="logo" alt="logo"/>
-        </div>
+        <div className="loadingAnimation"></div>
+        <img src={logo} className="logo" alt="logo" />
       </div>
-    )
+    );
   }
 }
 
 // Exporting Loading Animation
 export default LoadingAnimation;
-
-
