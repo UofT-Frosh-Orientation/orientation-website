712ef9c37c65973702b0bbe5ae6d3849055211d8
James <50821962+jameskokoska@users.noreply.github.com>
2022-06-09 19:19:02 -0400

    added other sections of profile

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { getInformation } from './functions';
+import {
+  getDaysFroshSchedule,
+  getFroshScheduleData,
+  getQRCodeString,
+  getTasks,
+  onDoneTask,
+} from './functions';
 import './Profile.scss';
 import WaveReverseFlip from '../../assets/misc/wave-reverse-flip.png';
+import { TaskAnnouncement } from '../../components/task/TaskAnnouncement/TaskAnnouncement';
+import { QRNormal } from 'react-qrbtf';
+import { ButtonBubble } from '../../components/button/ButtonBubble/ButtonBubble';
+import { Dropdown } from '../../components/form/Dropdown/Dropdown';
+import SingleAccordionStories from '../../components/text/Accordion/SingleAccordion/SingleAccordion.stories';
+import { SingleAccordion } from '../../components/text/Accordion/SingleAccordion/SingleAccordion';
 
 const PageProfile = () => {
   return (
     <>
       <div className="navbar-space-top" />
       <ProfilePageHeader />
+      <div className="profile-info-row">
+        <div>
+          <ProfilePageAnnouncements />
+          <ProfilePageSchedule />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <ProfilePageQRCode />
+          <ProfilePageResources />
+        </div>
+      </div>
     </>
   );
 };
@@ -33,7 +55,7 @@ const ProfilePageHeader = () => {
               </u>
             </p>
           </div>
-          <div className="profile-page-header-class">
+          <div className="profile-page-header-class desktop-only">
             <p>Class of</p>
             <h2>2T6</h2>
           </div>
@@ -42,6 +64,144 @@ const ProfilePageHeader = () => {
       <img src={WaveReverseFlip} className="wave-image home-page-bottom-wave-image" />
     </>
   );
+};
+
+const ProfilePageAnnouncements = () => {
+  return (
+    <div className="profile-page-announcements">
+      <h2 className="profile-page-section-header">Tasks and Announcements</h2>
+      <TaskAnnouncement tasks={getTasks()} onDone={onDoneTask} />
+    </div>
+  );
+};
+
+const ProfilePageQRCode = () => {
+  return (
+    <div className="profile-page-qr-code">
+      <QRNormal
+        value={getQRCodeString()}
+        styles={{ svg: { width: '120%', margin: '-10%' } }}
+        type="round"
+        opacity={100}
+        posType="rect"
+        otherColor="#320846"
+        posColor="#28093A"
+        backgroundColor="white"
+      />
+      <p>Your Sign-in QR Code</p>
+    </div>
+  );
+};
+const ProfilePageResources = () => {
+  return (
+    <div className="profile-page-resources">
+      <h2>Resources</h2>
+      <ButtonBubble
+        label="Campus Food Guide"
+        isSecondary
+        style={{ margin: 0, marginTop: '10px' }}
+      />
+      <ButtonBubble
+        label="Mental Health and Wellness Resources"
+        isSecondary
+        style={{ margin: 0, marginTop: '10px' }}
+      />
+    </div>
+  );
+};
+
+const ProfilePageSchedule = () => {
+  let days = getDaysFroshSchedule();
+  let scheduleData = getFroshScheduleData();
+  const today = new Date();
+  const options = { weekday: 'long' };
+  const todayString = today.toLocaleDateString('en-US', options).replace(',', '');
+  let count = 0;
+  for (let day of days) {
+    if (day === todayString) {
+      break;
+    }
+    count++;
+  }
+  if (count > scheduleData.length) {
+    count--;
+  }
+  const [selectedDayIndex, setSelectedDayIndex] = useState(count);
+  const [closeAll, setCloseAll] = useState(0);
+  return (
+    <div className="profile-page-schedule">
+      <h2 className="profile-page-section-header">Schedule</h2>
+      <div className="profile-page-schedule-buttons">
+        {days.map((item, index) => {
+          return (
+            <ButtonBubble
+              key={item + index}
+              label={item}
+              isSecondary={selectedDayIndex !== index}
+              style={{ flexGrow: 1, textAlign: 'center', maxWidth: '250px' }}
+              onClick={() => {
+                setSelectedDayIndex(index);
+                setCloseAll(!closeAll);
+              }}
+            />
+          );
+        })}
+      </div>
+      {scheduleData[selectedDayIndex].events.map((scheduleDateObj, index) => {
+        return (
+          <ProfilePageAccordionWrapper
+            key={scheduleDateObj.time + index}
+            closeAll={closeAll}
+            scheduleDateObj={scheduleDateObj}
+            index={index}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const ProfilePageAccordionWrapper = ({ scheduleDateObj, index, closeAll }) => {
+  let currentTime = new Date();
+  let hour = currentTime.getHours();
+  const meridian = hour > 12 ? 'PM' : 'AM';
+  hour = hour > 12 ? hour - 12 : hour;
+
+  let scheduleTime = scheduleDateObj.time;
+  const hourScheduleTime = scheduleTime.split(':')[0];
+  const meridianScheduleTime = scheduleTime.split(' ')[1];
+
+  const [isOpen, setIsOpen] = useState(
+    hour === hourScheduleTime && meridian === meridianScheduleTime,
+  );
+
+  useEffect(() => {
+    setIsOpen(hour === hourScheduleTime && meridian === meridianScheduleTime);
+  }, [closeAll]);
+  let accordionHeader = (
+    <div className="profile-page-accordion-header">
+      <h3>{scheduleDateObj.name}</h3>
+      <h4>{scheduleDateObj.time}</h4>
+    </div>
+  );
+  let accordionContent = (
+    <div className="profile-page-accordion-content">
+      <p>{scheduleDateObj.description}</p>
+    </div>
+  );
+  return (
+    <div className="profile-page-accordion-container">
+      <SingleAccordion isOpen={isOpen} setIsOpen={setIsOpen} header={accordionHeader}>
+        {accordionContent}
+      </SingleAccordion>
+    </div>
+  );
+};
+
+ProfilePageAccordionWrapper.propTypes = {
+  scheduleDateObj: PropTypes.object,
+  index: PropTypes.number,
+  closeAll: PropTypes.bool,
 };
 
 export { PageProfile };
--- client/src/pages/Profile/functions.jsx
+++ client/src/pages/Profile/functions.jsx
@@ -1,3 +1,99 @@
-export function getInformation() {
-  return 'FAQ';
+export function getTasks() {
+  return [
+    {
+      name: 'Welcome to your F!rosh profile page',
+      description:
+        "Take a look around and explore! You'll find your personalized Frosh group schedule and more information here. You can check this off when you're ready!",
+      dateCreated: new Date('2022-05-31T00:00:00'),
+      completed: true,
+    },
+    {
+      name: 'Welcome to your F!rosh profile page 2',
+      description: 'More talky talk and walky walk text...',
+      dateCreated: new Date('2022-05-31T00:00:00'),
+      completed: true,
+    },
+  ];
+}
+
+export function onDoneTask(task) {
+  console.log(task);
+}
+
+export function getFroshScheduleData() {
+  return [
+    {
+      date: 'Monday September 5', //This date format must be followed!
+      events: [
+        {
+          name: 'Meet your frosh group',
+          description: 'description',
+          time: '10:00 AM - 11:00 AM', //This time format must be followed!
+        },
+        {
+          name: 'Matriculation',
+          description: 'description',
+          time: '10:00 AM - 12:00 PM',
+        },
+      ],
+    },
+    {
+      date: 'Tuesday September 6',
+      events: [
+        {
+          name: 'Meet 1',
+          description: 'description',
+          time: '10:00 AM - 11:00 AM',
+        },
+        {
+          name: 'Meet 2',
+          description: 'description',
+          time: '10:00 AM - 12:00 PM',
+        },
+      ],
+    },
+    {
+      date: 'Wednesday September 7',
+      events: [
+        {
+          name: 'Meet 1',
+          description: 'description',
+          time: '10:00 AM - 11:00 AM',
+        },
+        {
+          name: 'Meet 2',
+          description: 'description',
+          time: '10:00 AM - 12:00 PM',
+        },
+      ],
+    },
+    {
+      date: 'Thursday September 8',
+      events: [
+        {
+          name: 'Meet 1',
+          description: 'description',
+          time: '10:00 AM - 11:00 AM',
+        },
+        {
+          name: 'Meet 2',
+          description: 'description',
+          time: '10:00 AM - 12:00 PM',
+        },
+      ],
+    },
+  ];
+}
+
+export function getDaysFroshSchedule() {
+  let scheduleData = getFroshScheduleData();
+  let days = [];
+  for (let day of scheduleData) {
+    days.push(day['date'].split(' ')[0]);
+  }
+  return days;
+}
+
+export function getQRCodeString() {
+  return 'Hello World';
 }
