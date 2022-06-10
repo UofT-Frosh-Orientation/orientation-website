a611887931b6c67f16f8920fca2d97e8664fe415
James <50821962+jameskokoska@users.noreply.github.com>
2022-06-10 11:42:20 -0400

    modified wrap and spacing

--- client/src/components/buttonSelector/buttonSelector/ButtonSelector.jsx
+++ client/src/components/buttonSelector/buttonSelector/ButtonSelector.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './ButtonSelector.scss';
 import { ButtonOutlined } from '../../button/ButtonOutlined/ButtonOutlined';
 
-const ButtonSelector = ({ buttonList, activeIndex, setActiveIndex }) => {
+const ButtonSelector = ({ buttonList, activeIndex, setActiveIndex, maxWidthButton }) => {
   function setActiveButton(index) {
     setActiveIndex(index);
   }
@@ -13,18 +13,19 @@ const ButtonSelector = ({ buttonList, activeIndex, setActiveIndex }) => {
       key={index}
       onClick={() => setActiveButton(index)}
       label={item.name}
-      style={{ backgroundColor: `${item.buttonColor}` }}
+      style={{ backgroundColor: `${item.buttonColor}`, flex: 1, maxWidth: maxWidthButton }}
       isSecondary={index == activeIndex ? false : true}
     ></ButtonOutlined>
   ));
 
-  return <div>{buttonItems}</div>;
+  return <div className="button-selector-items">{buttonItems}</div>;
 };
 
 ButtonSelector.propTypes = {
   buttonList: PropTypes.array.isRequired,
   activeIndex: PropTypes.number.isRequired,
   setActiveIndex: PropTypes.func.isRequired,
+  maxWidthButton: PropTypes.number,
 };
 
 export { ButtonSelector };
