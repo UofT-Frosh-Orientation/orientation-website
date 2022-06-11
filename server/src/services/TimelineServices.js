7c1294a2f1e2b2e1f9e9a2e90cbdcfe4353568b3
Emily Nguyen <emily.linh.nguyen@gmail.com>
2022-06-11 16:21:14 -0400

    get and update timeline element routes

--- server/src/services/TimelineServices.js
+++ server/src/services/TimelineServices.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt');
 const TimelineModel = require('../models/FroshModel');
 
 const FroshServices = {
+  //params timelineElement: [Model Element]
+  async updateTimelineElement(timelineElement) {
+    await timelineElement.save();
+  },
+  //params timelineElement: []
   async saveNewTimelineElement(timelineRecord) {
     const newTimelineElement = new TimelineModel(timelineRecord);
     await newTimelineElement.save();
