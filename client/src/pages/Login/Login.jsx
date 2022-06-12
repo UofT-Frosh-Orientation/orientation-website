6ee84a05a25983d9ad402aa4c2a7ffd87c39af75
Natalie Chan <natapoke2@gmail.com>
2022-06-11 23:17:08 -0400

    adding states for forgotten password popup, need to add states to close it

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './Login.scss';
 
@@ -14,41 +14,54 @@ import MountainFR from '../../assets/login/mountain-front-right.svg';
 import MountainM from '../../assets/login/mountain-mid.svg';
 import Ptero from '../../assets/login/ptero.svg';
 
+import XMark from '../../assets/misc/xmark-solid-white.svg';
+
 const PageLogin = ({ incorrectEntry }) => {
+  // pop up when clicking forget password
+  // deafult -- popup off
+  const [showPopUp, setShowPopUp] = useState(false);
+
   return (
-    <div className="login-entire-page">
-      <div className="login-bg">
-        <div className="login-container">
-          <h1 className="login-title">Login</h1>
-          <TextInput inputType={'text'} placeholder={'Email'}></TextInput>
-          <TextInput
-            inputType={'password'}
-            placeholder={'Password'}
-            errorFeedback={'Invalid email or password. Please try again!'}
-          ></TextInput>
+    <>
+      <div className="login-entire-page">
+        <div className="login-bg">
+          <div className="login-container">
+            <h1 className="login-title">Login</h1>
+            <TextInput inputType={'text'} placeholder={'Email'}></TextInput>
+            <TextInput
+              inputType={'password'}
+              placeholder={'Password'}
+              errorFeedback={'Invalid email or password. Please try again!'}
+            ></TextInput>
 
-          {/* Invalid Username or Password */}
-          {/* {incorrectEntry ? <p className="login-error-message">Incorrect Username or Password, please try again!</p> : <></>} */}
+            {/* Invalid Username or Password */}
+            {/* {incorrectEntry ? <p className="login-error-message">Incorrect Username or Password, please try again!</p> : <></>} */}
 
-          <div className="login-button-container">
-            <p className="forgot-message">Forgot Password?</p>
-            <Button label={'Log in'}></Button>
+            <div className="login-button-container">
+              <p
+                className="forgot-message"
+                onClick={() => setShowPopUp(true)}
+              >{`Forgot Password?`}</p>
+              <Button label={'Log in'}></Button>
+            </div>
           </div>
+        </div>
+
+        <div className="login-bg-images">
+          <img className="mountain-back" src={MountainB} alt="mountain"></img>
+          <img className="mountain-front-right" src={MountainFR} alt="mountain"></img>
+          <img className="mountain-mid" src={MountainM} alt="mountain"></img>
+          <img className="mountain-front-left" src={MountainFL} alt="mountain"></img>
+
+          <img className="ground" src={Ground} alt="ground"></img>
+          <img className="brachio-left" src={BrachioL} alt="brachiosaurus"></img>
+          <img className="brachio-right" src={BrachioR} alt="brachiosaurus"></img>
+          <img className="ptero" src={Ptero} alt="ptero"></img>
         </div>
       </div>
 
-      <div className="login-bg-images">
-        <img className="mountain-back" src={MountainB} alt="mountain"></img>
-        <img className="mountain-front-right" src={MountainFR} alt="mountain"></img>
-        <img className="mountain-mid" src={MountainM} alt="mountain"></img>
-        <img className="mountain-front-left" src={MountainFL} alt="mountain"></img>
-
-        <img className="ground" src={Ground} alt="ground"></img>
-        <img className="brachio-left" src={BrachioL} alt="brachiosaurus"></img>
-        <img className="brachio-right" src={BrachioR} alt="brachiosaurus"></img>
-        <img className="ptero" src={Ptero} alt="ptero"></img>
-      </div>
-    </div>
+      {showPopUp ? <PasswordPopUp /> : <></>}
+    </>
   );
 };
 
@@ -56,9 +69,35 @@ PageLogin.propTypes = {
   // if username/email/password are invalid
   incorrectEntry: PropTypes.bool,
 };
-
 PageLogin.defaultProps = {
   incorrectEntry: false,
+};
+
+const PasswordPopUp = () => {
+  const [showPopUp, setShowPopUp] = useState(true);
+
+  return (
+    <div className="forgot-password-popup">
+      <div className="forgot-password-container">
+        <img className="x-mark" src={XMark} alt="x-mark"></img>
+
+        <h2 className="reset-password-title">Reset Password</h2>
+        <p className="reset-password-des">{`Enter your email address below, and we'll send you an email to reset your password.`}</p>
+        <TextInput inputType={'text'} placeholder={'Email'}></TextInput>
+        <Button label={'Send'}></Button>
+
+        {/* when button is clicked message appears */}
+        <div className="reset-password-message-con">
+          <p className="reset-password-message"></p>
+        </div>
+        {/* We couldn't find your email, try again!  OR  Email sent successfully */}
+      </div>
+    </div>
+  );
+};
+
+PasswordPopUp.protoType = {
+  display: PropTypes.bool,
 };
 
 export { PageLogin };
