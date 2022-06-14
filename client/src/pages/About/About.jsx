1135be2340f72257286529071d9462b9988531a9
Natalie Chan <natapoke2@gmail.com>
2022-06-14 17:50:53 -0400

    layout is okay!

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -17,7 +17,7 @@ import profileWave from '../../assets/about/wave-about.svg';
 const PageAbout = () => {
   return (
     <>
-      <img className="aboutus-main-picture" src={Landing1}></img>
+      <AboutUsHeader />
       <div className="aboutus-page-components">
         <AboutUsSection />
         <h2 className="exec-title">Meet the Exec Team</h2>
@@ -30,10 +30,18 @@ const PageAbout = () => {
   );
 };
 
+const AboutUsHeader = () => {
+  return (
+    <div className="aboutus-header">
+      <img className="aboutus-main-picture" src={Landing1}></img>
+      <img className="wave-top" src={waveTop} alt="wave"></img>
+    </div>
+  );
+};
+
 const AboutUsSection = () => {
   return (
     <div className="aboutus-container">
-      <img className="wave-top" src={waveTop} alt="wave"></img>
       <div className="aboutus-subcontainer">
         <h2 className="aboutus-title">About Us</h2>
         <div className="aboutus-title-underline"></div>
@@ -66,32 +74,6 @@ const AboutUsSection = () => {
     </div>
   );
 };
-
-// const OCSection = () => {
-//   return (
-//     <div className="execteam-container">
-//       <h2 className="exec-title">Meet the Exec Team</h2>
-//       <div className="exec-title-underline"></div>
-
-//       {[execInfo.oc].map((info) => {
-//         return (
-//           <div className="oc-container" key={info.name}>
-//             <div className="oc-image"></div>
-//             <div className="oc-info">
-//               <div className="oc-title">
-//                 <h2 className="oc-info-name">{info.name}</h2>
-//                 <h3 className="oc-info-role">{info.role.toUpperCase()}</h3>
-//               </div>
-
-//               <p className="oc-info-des">{info.description}</p>
-//               <p className="oc-info-fav">{info.favPart}</p>
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
 
 const OCSection = () => {
   return (
@@ -134,30 +116,5 @@ const VCSection = () => {
     </div>
   );
 };
-
-// const VCSection = () => {
-//   return (
-//     <div className="vc-section-container">
-//       {[execInfo.vcs].map((info) => {
-//         return (
-//           <div className="exec-profile-container" key={info.role}>
-//             <div className="exec-profile-image"></div>
-//             <img className="profile-wave" src={profileWave}></img>
-//             <div className="exec-profile-title">
-//               <h2 className="exec-profile-position">{info.position}</h2>
-//               <h2 className="exec-profile-name">{info.name}</h2>
-//             </div>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
-
-// const ExecProfile = () => {
-//   return (
-
-//   );
-// };
 
 export { PageAbout };
