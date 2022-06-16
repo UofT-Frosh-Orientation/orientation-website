245a754c35b6fb731842a1eb0c11237800798d3a
Emily Nguyen <emily.linh.nguyen@gmail.com>
2022-06-16 12:12:33 -0400

    getTimeline Route

--- server/src/controllers/TimelineController.js
+++ server/src/controllers/TimelineController.js
@@ -8,12 +8,26 @@ app.use(express.static('public'));
 const YOUR_DOMAIN = 'http://localhost:5000';
 
 const TimelineController = {
+
+    async getTimeline(req, res, next) {
+      try{
+          const items = await TimelineModel
+            .find()
+            .lean()
+            .exec((err, items)=>{
+              if(!err){
+                  return res.json({items: items})
+              }
+            }); 
+      } catch(e) {
+        next(e);
+      }
+    },
+
     getTimelineElement(req, res, next) {
       let timelineData = req.body;
       let id = req.params.id;
       try {
-        //need to change for permissions - which users can access
-        await TimelineServices.validateUser(timelineData);
         // have a form that automatically fills out fields with current model information
         // gets corresponding timelineelement informaiton to prefill form -> reroutes to edit form
         res.render("/:id/edit",
@@ -29,8 +43,9 @@ const TimelineController = {
     editTimelineElement(req, res, next) {
       let timelineData = req.body;
       try {
-        // have a form that automatically fills out fields with current model information
+        //need to change for permissions - which users can access
         await TimelineServices.validateUser(timelineData);
+        // have a form that automatically fills out fields with current model information
         timelineRecord = {
           date: timelineData.date,
           name: timelineData.name,
@@ -38,21 +53,25 @@ const TimelineController = {
         }
         await TimelineServices.updateTimelineElement(timelineRecord);
         res.status(200).send({ message: 'Successfully updated timeline element!' });
+        res.redirect('/timeline/:id');
       } catch (e) {
         next(e);
       }
     },
 
-    addTimeline(req, res, next) {
-      let timelineId = req.body;
+    addTimelineElement(req, res, next) {
+      let timelineData = req.body;
       try {
+        //need to change for permissions - which users can access
+        await TimelineServices.validateUser(timelineData);
         timelineRecord = {
           date: timelineData.date,
           name: timelineData.name,
           description: timelineData.description
-        }
+        };
         await TimelineServices.saveNewTimelineElement(timelineRecord);
         res.status(200).send({ message: 'Successfully added new timeline element!' });
+        res.redirect('/timeline');
       } catch (e) {
         next(e);
       }
@@ -70,8 +89,13 @@ const TimelineController = {
         }
         await TimelineServices.saveNewTimelineElement(timelineRecord);
         res.status(200).send({ message: 'Successfully deleted timeline element!' });
+        res.redirect('/timeline/:id');
       } catch (e) {
         next(e);
       }
+    }, 
+
+    changeTimelineOrder(req, res, next) {
+      
     }
 };
