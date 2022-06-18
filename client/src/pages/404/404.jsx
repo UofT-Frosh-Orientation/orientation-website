2e1fabc206e2da87457fa2da98421856392c6aa8
James <50821962+jameskokoska@users.noreply.github.com>
2022-06-18 13:48:29 -0400

    added 404 page

--- client/src/pages/404/404.jsx
+++ client/src/pages/404/404.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DinoBones from '../../assets/404/DinoBones.svg';
+import Grass from '../../assets/404/grass.png';
+import Meteor from '../../assets/404/meteor.png';
+import './404.scss';
 
 const Page404 = () => {
   return (
-    <div style={{ backgroundColor: 'orange', width: '100px', height: '100px' }}>
-      {'Error 404. Page not found.'}
+    <div className="error404-container">
+      <div className="error404-sky" />
+      <div style={{ backgroundImage: `url(${Grass})` }} className="error404-grass" />
+      <img src={DinoBones} className="error404-bg-image"></img>
+      <img src={Meteor} className="error404-meteor-image"></img>
+      <div style={{ margin: 'auto auto' }}>
+        <h1>Error 404</h1>
+        <h2>Page not found</h2>
+      </div>
     </div>
   );
 };
