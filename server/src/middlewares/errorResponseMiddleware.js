12faaa9232e8259d116e8094f60dff34dcff3873
Cali0707 <calumramurray@gmail.com>
2022-06-20 11:42:20 -0400

    Small fix for error handling

--- server/src/middlewares/errorResponseMiddleware.js
+++ server/src/middlewares/errorResponseMiddleware.js
@@ -4,7 +4,7 @@ function errorResponseMiddleware(err, req, res, next) {
   if (err.message === 'INVALID_EMAIL') {
     statusCode = 400;
     errorMessage = 'Please provide a valid email!';
-  } else if (error.message === 'UNAUTHORIZED') {
+  } else if (err.message === 'UNAUTHORIZED') {
     statusCode = 401;
     errorMessage = 'Unauthorized';
   }
