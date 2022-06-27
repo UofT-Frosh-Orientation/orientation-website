fc66acce0473d66670adc78355a3a3a2fc5018a2
Emily Nguyen <emily.linh.nguyen@gmail.com>
2022-06-26 22:58:57 -0400

    incorporate promises

--- server/src/services/AnnouncementServices.js
+++ server/src/services/AnnouncementServices.js
@@ -1,19 +1,64 @@
 const AnnouncementModel = require('../models/AnnouncementModel');
 
 const AnnouncementServices = {
-  //params announcementElement: [Model Element]
-  async updateAnnouncementElement(id, announcementElement) {
-    return await AnnouncementModel.findOneAndUpdate({ _id: id }, announcementElement);
+  async getAllAnnouncements() {
+    return new Promise((resolve, reject) => {
+      AnnouncementModel.find({}, (err, announcements) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(announcements);
+        }
+      });
+    });
   },
-  //params announcementElement: []
-  async saveNewAnnouncementElement(announcementElement) {
-    const announcementModel = new AnnouncementModel(announcementElement);
 
-    return await announcementModel.save();
+  async getAnnouncementElement(id) {
+    return new Promise((resolve, reject) => {
+      AnnouncementModel.findOne({ _id: id }, (err, announcement) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(announcement);
+        }
+      });
+    });
+  },
+
+  async updateAnnouncementElement(id, announcementElement) {
+    return new Promise((resolve, reject) => {
+      AnnouncementModel.findOneAndUpdate({ _id: id }, announcementElement, (err, announcement) => {
+        if (err || !announcement) {
+          reject('UNABLE_TO_UPDATE_ANNOUNCEMENT');
+        } else {
+          resolve(announcement);
+        }
+      });
+    });
+  },
+
+  async saveNewAnnouncementElement(announcementElement) {
+    return new Promise((resolve, reject) => {
+      AnnouncementModel.create(announcementElement, (err, newAnnouncement) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(newAnnouncement);
+        }
+      });
+    });
   },
 
   async deleteAnnouncementElement(id) {
-    return await AnnouncementModel.findOneAndDelete({ _id: id });
+    return new Promise((resolve, reject) => {
+      AnnouncementModel.findOneAndDelete({ _id: id }, (err, deleteAnnouncement) => {
+        if (err || !deleteAnnouncement) {
+          reject('UNABLE_TO_DELETE_ANNOUNCEMENT');
+        } else {
+          resolve(deleteAnnouncement);
+        }
+      });
+    });
   },
 };
 
