90bebe5006f51177659388852c29266d4734efcf
Cali0707 <calumramurray@gmail.com>
2022-06-29 20:10:18 -0400

    Updated initial landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,51 @@
-import { BrowserRouter, Link, useLocation, Route, Routes } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import ScrollToTop from './components/misc/ScrollToTop/ScrollToTop';
+// import { BrowserRouter, Link, useLocation, Route, Routes } from 'react-router-dom';
+// import { TransitionGroup, CSSTransition } from 'react-transition-group';
+// import ScrollToTop from './components/misc/ScrollToTop/ScrollToTop';
 import './App.scss';
 import { InitialPage } from './pages/Initial/Initial';
-import { pages } from './util/pages';
-import { Navbar } from './components/Navbar/Navbar';
-import { Footer } from './components/footer/Footer';
+// import { pages } from './util/pages';
+// import { Navbar } from './components/Navbar/Navbar';
+// import { Footer } from './components/footer/Footer';
 
 export default function App() {
-  const initial = import.meta.env.PROD;
-  if (initial) {
-    return <InitialPage />;
-  } else {
-    return (
-      <BrowserRouter>
-        <TransitionRoutes />
-      </BrowserRouter>
-    );
-  }
+  return <InitialPage />;
+  // const initial = true;
+  // if (initial) {
+  //   return <InitialPage />;
+  // } else {
+  //   return (
+  //     <BrowserRouter>
+  //       <TransitionRoutes />
+  //     </BrowserRouter>
+  //   );
+  // }
 }
-
-const TransitionRoutes = () => {
-  const location = useLocation();
-  return (
-    <TransitionGroup>
-      <Navbar />
-      <ScrollToTop />
-      <CSSTransition key={location.key} classNames="page" timeout={300}>
-        <Routes location={location}>
-          {[...pages.main, ...pages.hidden].map((page) => {
-            return (
-              <Route
-                path={page.path}
-                key={page.path}
-                element={
-                  <div style={{ position: 'absolute', right: 0, left: 0, bottom: 0, top: 0 }}>
-                    <div style={{ minHeight: '100vh' }}>{page.component}</div>
-                    <Footer />
-                  </div>
-                }
-              />
-            );
-          })}
-          <Route path="*" element={pages['404'].component} />
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-};
+//
+// const TransitionRoutes = () => {
+//   const location = useLocation();
+//   return (
+//     <TransitionGroup>
+//       <Navbar />
+//       <ScrollToTop />
+//       <CSSTransition key={location.key} classNames="page" timeout={300}>
+//         <Routes location={location}>
+//           {[...pages.main, ...pages.hidden].map((page) => {
+//             return (
+//               <Route
+//                 path={page.path}
+//                 key={page.path}
+//                 element={
+//                   <div style={{ position: 'absolute', right: 0, left: 0, bottom: 0, top: 0 }}>
+//                     <div style={{ minHeight: '100vh' }}>{page.component}</div>
+//                     <Footer />
+//                   </div>
+//                 }
+//               />
+//             );
+//           })}
+//           <Route path="*" element={pages['404'].component} />
+//         </Routes>
+//       </CSSTransition>
+//     </TransitionGroup>
+//   );
+// };
