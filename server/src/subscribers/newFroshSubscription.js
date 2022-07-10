1e3e2cbf5855d30de74d596a8f3194a76f200e19
Freeassassin <farbodemzadeh@gmail.com>
2022-07-09 21:51:27 -0400

    WIP

--- server/src/subscribers/newFroshSubscription.js
+++ server/src/subscribers/newFroshSubscription.js
@@ -1,4 +1,5 @@
 const Queue = require('bull');
+const EmailServices = require('../services/EmailServices');
 
 const newFroshSubscription = new Queue('newFrosh', {
   redis: { port: process.env.REDIS_PORT, host: 'redis', password: process.env.REDIS_PASSWORD },
@@ -7,5 +8,21 @@ const newFroshSubscription = new Queue('newFrosh', {
 newFroshSubscription.process((job, done) => {
   console.log('Registered frosh!');
   console.log(job.data);
-  done();
+  try {
+    // sending user creation email
+    const result = EmailServices.sendTemplateEmail(
+      {},
+      'registration_confirmation',
+      [job.data.email],
+      'tech@orientation.skule.ca',
+    );
+
+    result.then((response) => {
+      console.log(response);
+    });
+
+    done();
+  } catch (error) {
+    done(error);
+  }
 });
