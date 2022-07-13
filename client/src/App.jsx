f229628765bbc2bc7b10f88a77a8a6f5eb310aca
Cali0707 <calumramurray@gmail.com>
2022-07-12 21:38:34 -0400

    Beta fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
-import { BrowserRouter, Link, useLocation, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, useLocation, Route, Routes } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import ScrollToTop from './components/misc/ScrollToTop/ScrollToTop';
 import './App.scss';
-import { InitialPage } from './pages/Initial/Initial';
 import { pages } from './util/pages';
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/footer/Footer';
