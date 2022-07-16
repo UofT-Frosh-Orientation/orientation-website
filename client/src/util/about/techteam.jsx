ae0cf1dd106ae2d68fa88346e5a90d04a0c56de0
Natalie Chan <natapoke2@gmail.com>
2022-07-16 00:10:37 -0400

    forgot to add the untracked files :')

---
+++ client/src/util/about/techteam.jsx
@@ -0,0 +1,45 @@
+import calum from '../../assets/about/techteam/calum.jpg';
+import emily from '../../assets/about/techteam/emily.jpg';
+import farbod from '../../assets/misc/dino-placeholder.svg';
+import james from '../../assets/about/techteam/james.jpg';
+import luke from '../../assets/about/techteam/luke.jpg';
+import neo from '../../assets/about/techteam/neo.jpg';
+
+export const techTeam = [
+  {
+    fullName: 'Calum Murray',
+    discipline: 'EngSci ECE 2T4',
+    quote: 'Tech team best team',
+    img: calum,
+  },
+  {
+    fullName: 'Emily Nguyen',
+    discipline: 'EngSci MI 2T4',
+    quote: 'Join BrewofT and brew ur own spicy water!',
+    img: emily,
+  },
+  {
+    fullName: 'Farbod Mohammadzadeh',
+    discipline: 'EngSci 2T5',
+    quote: 'tech team best subcom!',
+    img: farbod,
+  },
+  {
+    fullName: 'James Kokoska',
+    discipline: 'CE 2T2',
+    quote: "This website's repo is open source :)",
+    img: james,
+  },
+  {
+    fullName: 'Luke Yang',
+    discipline: 'EngSci ROBO 2T4',
+    quote: "Help I'm addicted to Valorant",
+    img: luke,
+  },
+  {
+    fullName: 'Neo Lou',
+    discipline: 'EngSci ECE 2T4',
+    quote: "I don't like bugs, both physical and digital ones",
+    img: neo,
+  },
+];
