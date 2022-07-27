c2b6553d66dfaad51cd120422d3e2d4d42a9d48b
James <50821962+jameskokoska@users.noreply.github.com>
2022-07-27 17:12:43 -0400

    added judge exec panel

--- client/src/pages/ScuntJudgeForm/ScuntJudgeForm.jsx
+++ client/src/pages/ScuntJudgeForm/ScuntJudgeForm.jsx
@@ -11,10 +11,11 @@ import { Dropdown } from '../../components/form/Dropdown/Dropdown';
 import { Button } from '../../components/button/Button/Button';
 import { ErrorSuccessBox } from '../../components/containers/ErrorSuccessBox/ErrorSuccessBox';
 import { QRScannerDisplay } from '../../components/QRScannerDisplay/QRScannerDisplay';
+import { ButtonOutlined } from '../../components/button/ButtonOutlined/ButtonOutlined';
+import { PopupModal } from '../../components/popup/PopupModal';
 
 export const PageScuntJudgeForm = () => {
   const { user } = useSelector(userSelector);
-  const [remainingBribePoints, setRemainingBribePoints] = useState(500);
   const teams = [
     'Team 1',
     'Team 2',
@@ -42,18 +43,168 @@ export const PageScuntJudgeForm = () => {
           </h3>
           <ScuntMissionSelection teams={teams} missions={list} />
           <div className="separator" />
-          <ScuntBribePoints
-            setRemainingBribePoints={setRemainingBribePoints}
-            remainingBribePoints={remainingBribePoints}
-            teams={teams}
-          />
+          <ScuntBribePoints teams={teams} />
         </div>
       </div>
+      <ScuntExecDashboard teams={teams} />
     </>
   );
 };
 
-const ScuntBribePoints = ({ teams, remainingBribePoints, setRemainingBribePoints }) => {
+const ScuntExecDashboard = ({ teams }) => {
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [action, setAction] = useState('');
+  const [actionResultsError, setActionResultsError] = useState('');
+  const [actionResultsSuccess, setActionResultsSuccess] = useState('');
+  const timerRef = useRef(null);
+  const [amountOfRefillPoints, setAmountOfRefillPoints] = useState(0);
+  const [amountOfRemovePoints, setAmountOfRemovePoints] = useState(0);
+
+  const confirmAction = (action) => {
+    setActionResultsError('');
+    setActionResultsSuccess('');
+    setShowPopUp(true);
+    setAction(action);
+  };
+
+  const performAction = async (action) => {
+    clearTimeout(timerRef.current);
+
+    if (action === 'Allow Missions Page') {
+      console.log('allow missions page');
+    } else if (action === 'Hide Missions Page') {
+      console.log('hide missions page');
+    } else if (action === 'Show Wedding Missions') {
+      console.log('show wedding missions');
+    } else if (action === 'Hide Wedding Missions') {
+      console.log('hide wedding missions');
+    } else if (action === 'Allow Leaderboard') {
+      console.log('allow leaderboard');
+    } else if (action === 'Hide Leaderboard') {
+      console.log('hide leaderboard');
+    } else if (action === 'Refill') {
+      console.log(amountOfRefillPoints);
+    } else if (action === 'Remove Points') {
+      console.log(amountOfRemovePoints);
+    }
+
+    setActionResultsSuccess('Success: ' + action);
+    setTimeout(() => {
+      setActionResultsError('Error: ' + action);
+    }, 1000);
+
+    setShowPopUp(false);
+    timerRef.current = setTimeout(() => {
+      setActionResultsError('');
+      setActionResultsSuccess('');
+    }, 5000);
+  };
+
+  return (
+    <>
+      <div className="scunt-judge-form-page">
+        <div className="scunt-judge-form-container">
+          <h1>Exec Dashboard</h1>
+
+          <div className="scunt-exec-dashboard">
+            <div style={{ height: '15px' }} />
+
+            <h2>Refill Bribe Points</h2>
+            <div style={{ height: '5px' }} />
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Dropdown
+                values={['Judge 1', 'Judge 2']}
+                initialSelectedIndex={0}
+                onSelect={() => {}}
+              />
+              <div className="fill-remaining-width-input">
+                <TextInput
+                  placeholder={'# Points'}
+                  onChange={(value) => {
+                    setAmountOfRefillPoints(value);
+                  }}
+                />
+              </div>
+            </div>
+            <Button label={'Refill'} onClick={() => confirmAction('Refill')} />
+
+            <div style={{ height: '15px' }} />
+            <h2>Negative Points</h2>
+            <div style={{ height: '5px' }} />
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Dropdown values={teams} initialSelectedIndex={0} onSelect={() => {}} />
+              <div className="fill-remaining-width-input">
+                <TextInput
+                  placeholder={'# Points'}
+                  onChange={(value) => {
+                    setAmountOfRemovePoints(value);
+                  }}
+                />
+              </div>
+            </div>
+            <Button label={'Remove Points'} onClick={() => confirmAction('Remove Points')} />
+
+            <div style={{ height: '15px' }} />
+            <h2>Controls</h2>
+            <div style={{ height: '5px' }} />
+            <Button
+              label={'Allow Missions Page'}
+              onClick={() => confirmAction('Allow Missions Page')}
+            />
+            <ButtonOutlined
+              label={'Hide Missions Page'}
+              onClick={() => confirmAction('Hide Missions Page')}
+            />
+            <Button
+              label={'Show Wedding Missions'}
+              onClick={() => confirmAction('Show Wedding Missions')}
+            />
+            <ButtonOutlined
+              label={'Hide Wedding Missions'}
+              onClick={() => confirmAction('Hide Wedding Missions')}
+            />
+            <Button
+              label={'Allow Leaderboard'}
+              onClick={() => confirmAction('Allow Leaderboard')}
+            />
+            <ButtonOutlined
+              label={'Hide Leaderboard'}
+              onClick={() => confirmAction('Hide Leaderboard')}
+            />
+            <div className="error-success-box-fixed">
+              <ErrorSuccessBox content={actionResultsError} error={true} />
+              <ErrorSuccessBox content={actionResultsSuccess} success={true} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <PopupModal
+        trigger={showPopUp}
+        setTrigger={setShowPopUp}
+        blurBackground={false}
+        exitIcon={true}
+      >
+        <div className="scunt-exec-dashboard-popup">
+          <h2>Are you sure?</h2>
+          <h3>{action}</h3>
+          <Button
+            label={'Yes'}
+            onClick={() => {
+              performAction(action);
+            }}
+          ></Button>
+        </div>
+      </PopupModal>
+    </>
+  );
+};
+
+ScuntExecDashboard.propTypes = {
+  teams: PropTypes.array,
+};
+
+const ScuntBribePoints = ({ teams }) => {
+  const [remainingBribePoints, setRemainingBribePoints] = useState(500);
   const [assignedPoints, setAssignedPoints] = useState(0);
   const [assignedTeam, setAssignedTeam] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
@@ -131,7 +282,7 @@ const ScuntBribePoints = ({ teams, remainingBribePoints, setRemainingBribePoints
           </div>
           <h2 style={{ textAlign: 'center' }}>{assignedTeam}</h2>
           <h3 style={{ textAlign: 'center' }}>{assignedPoints} Points</h3>
-          <div style={{ width: '100%' }}>
+          <div className="error-success-box-fixed">
             <ErrorSuccessBox content={errorMessage} error={true} />
             <ErrorSuccessBox content={successMessage} success={true} />
           </div>
@@ -143,8 +294,6 @@ const ScuntBribePoints = ({ teams, remainingBribePoints, setRemainingBribePoints
 
 ScuntBribePoints.propTypes = {
   teams: PropTypes.array,
-  remainingBribePoints: PropTypes.number,
-  setRemainingBribePoints: PropTypes.func,
 };
 
 const ScuntMissionSelection = ({ missions, teams }) => {
@@ -356,7 +505,7 @@ const ScuntMissionSelection = ({ missions, teams }) => {
       ) : (
         <></>
       )}
-      <div style={{ width: '100%' }}>
+      <div className="error-success-box-fixed">
         <ErrorSuccessBox content={errorMessage} error={true} />
         <ErrorSuccessBox content={successMessage} success={true} />
       </div>
