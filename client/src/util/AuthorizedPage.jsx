d81f56a9365b425a93ce8ebd3bb511502cebb7af
Cali0707 <calumramurray@gmail.com>
2022-08-02 18:08:44 -0400

    Added auth scopes to authorized page

--- client/src/util/AuthorizedPage.jsx
+++ client/src/util/AuthorizedPage.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { loggedInSelector } from '../state/user/userSlice';
+import { loggedInSelector, userSelector } from '../state/user/userSlice';
 import { getUserInfo } from '../state/user/saga';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const AuthorizedPage = ({ children }) => {
+const AuthorizedPage = ({ children, authScopes = [] }) => {
   const loggedIn = useSelector(loggedInSelector);
+  const { user } = useSelector(userSelector);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -19,14 +20,19 @@ const AuthorizedPage = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    console.log(`Logged in: ${loggedIn}`);
-  }, [loggedIn]);
+    authScopes.forEach((auth) => {
+      if (!user?.authScopes?.approved?.includes(auth)) {
+        navigate('/');
+      }
+    });
+  }, [user]);
 
   return <>{loggedIn && children}</>;
 };
 
 AuthorizedPage.propTypes = {
   children: PropTypes.node.isRequired,
+  authScopes: PropTypes.array,
 };
 
 export default AuthorizedPage;
