cff6e888f829082bc053564266f336b2a2542341
jameskokoska <50821962+jameskokoska@users.noreply.github.com>
2022-08-05 01:07:15 -0400

    added scunt scopes

--- client/src/pages/ScopeRequest/functions.jsx
+++ client/src/pages/ScopeRequest/functions.jsx
@@ -3,7 +3,23 @@ const { axios } = useAxios();
 import { fields, terms } from '../Registration/RegistrationFields';
 
 export const getTotalScopes = () => {
-  return { faq: ['read', 'edit'], announcements: ['read', 'edit'], signInFrosh: [true] };
+  return {
+    faq: ['read', 'edit'],
+    announcements: ['read', 'edit'],
+    signInFrosh: ['qr-code registration'],
+    scunt: [
+      'judge missions',
+      'judge bribe points',
+      'exec refill bribe points',
+      'exec negative points',
+      'exec allow missions page',
+      'exec hide missions page',
+      'exec show wedding missions',
+      'exec hide wedding missions',
+      'exec allow leaderboard',
+      'exec hide leaderboard',
+    ],
+  };
 };
 
 export const getTotalRegistrationScopes = () => {
