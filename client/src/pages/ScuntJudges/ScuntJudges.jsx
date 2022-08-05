31534592be78b79cad0cb51d4ba6b43c9c4c2139
Natalie Chan <natapoke2@gmail.com>
2022-08-04 23:31:58 -0400

    added local storage variable array for profile clicks

--- client/src/pages/ScuntJudges/ScuntJudges.jsx
+++ client/src/pages/ScuntJudges/ScuntJudges.jsx
@@ -14,14 +14,18 @@ const ScuntJudges = () => {
   const [openPopup, setOpenPopup] = useState(true);
   const [showTechTeam, setShowTechTeam] = useState(false); // shows tech team image
   const [showTechTeamPopup, setShowTechTeamPopup] = useState(false); // shows secret judges revealed popup
+  const [profileClicks, setProfileClicks] = useState([{ name: 'test', clicks: 0 }]);
 
   useLayoutEffect(() => {
     // do this first!
     const popupdata = window.localStorage.getItem('scunt-judges-popup');
     const showtechteamdata = window.localStorage.getItem('show-tech-team-secret-judge');
+    const scuntprofiledata = window.localStorage.getItem('scunt-judges-profile');
+
     if (popupdata !== null) {
       setOpenPopup(JSON.parse(popupdata));
       setShowTechTeam(JSON.parse(showtechteamdata));
+      setProfileClicks(JSON.parse(scuntprofiledata));
     }
   }, []);
 
@@ -43,6 +47,10 @@ const ScuntJudges = () => {
   useEffect(() => {
     window.localStorage.setItem('show-tech-team-secret-judge', JSON.stringify(showTechTeam));
   }, [showTechTeam]);
+
+  useEffect(() => {
+    window.localStorage.setItem('scunt-judges-profile', JSON.stringify(profileClicks));
+  }, [profileClicks]);
 
   useEffect(() => {
     if (totalClicks === clicks) {
@@ -71,21 +79,42 @@ const ScuntJudges = () => {
           const [numClicks, setNumClicks] = useState(0);
 
           useEffect(() => {
-            const data = window.localStorage.getItem('Profile-Clicks-Scunt-Judges-' + judge.name);
-            if (data !== null) {
-              setNumClicks(JSON.parse(data));
+            // getting numclicks from local storage
+            const data = window.localStorage.getItem('scunt-judges-profile');
+            let updateData = JSON.parse(data);
+
+            let index = updateData.findIndex((i) => i.name === judge.name);
+
+            if (index !== -1) {
+              // not in array
+              setNumClicks(updateData[index].clicks);
             }
           }, []);
 
           useEffect(() => {
-            window.localStorage.setItem(
-              'Profile-Clicks-Scunt-Judges-' + judge.name,
-              JSON.stringify(numClicks),
-            );
+            // updating local storage with clicks
+            const data = window.localStorage.getItem('scunt-judges-profile');
+            let updateData = JSON.parse(data);
+            //console.log(updateData);
+
+            let index = updateData.findIndex((i) => i.name === judge.name);
+
+            if (index === -1) {
+              // not in array
+              let obj = { name: judge.name, clicks: numClicks };
+
+              updateData.push(obj);
+            } else {
+              updateData[index].clicks = numClicks;
+            }
+
+            window.localStorage.setItem('scunt-judges-profile', JSON.stringify(updateData));
+
             if (numClicks === 1 && clickProfile) {
               setTotalClicks(totalClicks + 1);
             }
           }, [numClicks]);
+
           if (judge.name === 'Tech Team') {
             if (showTechTeam) {
               return (
