b2057a02f96d1b24c751cb70560463bc80b598c4
Freeassassin <farbodemzadeh@gmail.com>
2022-08-07 21:30:15 -0400

    set up announcements redux

--- client/src/state/announcements/announcementsSlice.jsx
+++ client/src/state/announcements/announcementsSlice.jsx
@@ -5,6 +5,7 @@ const initialState = {
   loading: false,
   error: null,
   announcements: [],
+  completedAnnouncements: [],
 };
 
 const announcementSlice = createSlice({
@@ -24,11 +25,87 @@ const announcementSlice = createSlice({
       state.loading = false;
       state.error = error;
     },
+    getCompletedAnnouncementsStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    getCompletedAnnouncementsSuccess: (state, { payload: completeAnnouncement }) => {
+      state.loading = false;
+      state.error = null;
+      state.completeAnnouncement = completeAnnouncement;
+    },
+    getCompletedAnnouncementsFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+    createAnnouncementsStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    createAnnouncementsFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+    createAnnouncementsSuccess: (state) => {
+      state.loading = false;
+      state.error = null;
+    },
+    completeAnnouncementsStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    completeAnnouncementsFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+    completeAnnouncementsSuccess: (state) => {
+      state.loading = false;
+      state.error = null;
+    },
+    editAnnouncementsStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    editAnnouncementsFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+    editAnnouncementsSuccess: (state) => {
+      state.loading = false;
+      state.error = null;
+    },
+    deleteAnnouncementsStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteAnnouncementsFailure: (state, { payload: error }) => {
+      state.loading = false;
+      state.error = error;
+    },
+    deleteAnnouncementsSuccess: (state) => {
+      state.loading = false;
+      state.error = null;
+    },
   },
 });
 
-export const { getAnnouncementsStart, getAnnouncementsSuccess, getAnnouncementsFailure } =
-  announcementSlice.actions;
+export const {
+  getAnnouncementsStart,
+  getAnnouncementsSuccess,
+  getAnnouncementsFailure,
+  createAnnouncementsStart,
+  createAnnouncementsFailure,
+  createAnnouncementsSuccess,
+  completeAnnouncementsStart,
+  completeAnnouncementsFailure,
+  completeAnnouncementsSuccess,
+  editAnnouncementsStart,
+  editAnnouncementsFailure,
+  editAnnouncementsSuccess,
+  deleteAnnouncementsStart,
+  deleteAnnouncementsFailure,
+  deleteAnnouncementsSuccess,
+} = announcementSlice.actions;
 
 export default announcementSlice.reducer;
 
@@ -37,4 +114,9 @@ export const announcementReducerSelector = (state) => state[announcementSlice.na
 export const announcementsSelector = createSelector(
   announcementReducerSelector,
   ({ announcements, loading, error }) => ({ announcements, loading, error }),
+);
+
+export const completedAnnouncementsSelector = createSelector(
+  announcementReducerSelector,
+  ({ completedAnnouncements, loading, error }) => ({ completedAnnouncements, loading, error }),
 );
--- client/src/state/announcements/saga.jsx
+++ client/src/state/announcements/saga.jsx
@@ -5,6 +5,18 @@ import {
   getAnnouncementsStart,
   getAnnouncementsSuccess,
   getAnnouncementsFailure,
+  createAnnouncementsStart,
+  createAnnouncementsFailure,
+  createAnnouncementsSuccess,
+  completeAnnouncementsStart,
+  completeAnnouncementsFailure,
+  completeAnnouncementsSuccess,
+  editAnnouncementsStart,
+  editAnnouncementsFailure,
+  editAnnouncementsSuccess,
+  deleteAnnouncementsStart,
+  deleteAnnouncementsFailure,
+  deleteAnnouncementsSuccess,
 } from './announcementSlice';
 
 export const getAnnouncements = createAction('getAnnouncementsSaga');
@@ -20,6 +32,90 @@ export function* getAnnouncementsSaga() {
   }
 }
 
+export const createAnnouncements = createAction('createAnnouncementsSaga');
+
+export function* createAnnouncementsSaga({ payload: { setSnackbar, announcementData } }) {
+  const { axios } = useAxios();
+  try {
+    yield put(createAnnouncementsStart());
+    const response = yield call(axios.post, `/announcements/create`, { announcementData });
+    yield put(createAnnouncementsSuccess());
+  } catch (e) {
+    setSnackbar(
+      e.response?.data?.message
+        ? e.response?.data?.message.toString()
+        : e.response?.data
+        ? e.response?.data.toString()
+        : e.toString(),
+      true,
+    );
+    yield put(createAnnouncementsFailure(e));
+  }
+}
+
+export const completeAnnouncements = createAction('completeAnnouncementsSaga');
+
+export function* completeAnnouncementsSaga({ payload: { announcementData } }) {
+  const { axios } = useAxios();
+  try {
+    yield put(completeAnnouncementsStart());
+    const response = yield call(axios.put, `/announcements/${announcementData.id}/complete`, {
+      announcementData,
+    });
+    yield put(completeAnnouncementsSuccess());
+  } catch (e) {
+    yield put(completeAnnouncementsFailure(e));
+  }
+}
+
+export const editAnnouncement = createAction('editAnnouncementSaga');
+
+export function* editAnnouncementSaga({ payload: { setSnackbar, announcementData } }) {
+  const { axios } = useAxios();
+  try {
+    yield put(editAnnouncementsStart());
+    const response = yield call(axios.put, `/announcements/${announcementData.id}/edit`, {
+      announcementData,
+    });
+    yield put(editAnnouncementsSuccess());
+  } catch (e) {
+    setSnackbar(
+      e.response?.data?.message
+        ? e.response?.data?.message.toString()
+        : e.response?.data
+        ? e.response?.data.toString()
+        : e.toString(),
+      true,
+    );
+    yield put(editAnnouncementsFailure(e));
+  }
+}
+
+export const deleteAnnouncement = createAction('deleteAnnouncementSaga');
+
+export function* deleteAnnouncementSaga({ payload: { setSnackbar, announcementData } }) {
+  const { axios } = useAxios();
+  try {
+    yield put(deleteAnnouncementsStart());
+    const response = yield call(axios.delete, `/announcements/${announcementData.id}/delete`);
+    yield put(deleteAnnouncementsSuccess());
+  } catch (e) {
+    setSnackbar(
+      e.response?.data?.message
+        ? e.response?.data?.message.toString()
+        : e.response?.data
+        ? e.response?.data.toString()
+        : e.toString(),
+      true,
+    );
+    yield put(deleteAnnouncementsFailure(e));
+  }
+}
+
 export default function* announcementsSaga() {
   yield takeLeading(getAnnouncements.type, getAnnouncementsSaga);
+  yield takeLeading(createAnnouncements.type, createAnnouncementsSaga);
+  yield takeLeading(completeAnnouncements.type, completeAnnouncementsSaga);
+  yield takeLeading(editAnnouncement.type, editAnnouncementSaga);
+  yield takeLeading(deleteAnnouncement.type, deleteAnnouncementSaga);
 }
