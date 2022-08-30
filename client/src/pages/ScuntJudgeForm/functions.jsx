6a0d4e7fd7d32b2eb4dd1b17ea4fd0561ced11e9
NeoLou <neolou2000@hotmail.com>
2022-08-29 20:41:16 -0400

    connected to backend judge submitting bribe, has not tested yet

--- client/src/pages/ScuntJudgeForm/functions.jsx
+++ client/src/pages/ScuntJudgeForm/functions.jsx
@@ -1,2 +1,15 @@
 import useAxios from '../../hooks/useAxios.jsx';
 const { axios } = useAxios();
+
+export async function submitBribePoints(req) {
+  // req consists of an object with properties "teamNumber" and "points"
+  console.log(req);
+  try {
+    const response = await axios.post('/scunt-teams/transaction/bribe', req);
+    console.log(response);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
