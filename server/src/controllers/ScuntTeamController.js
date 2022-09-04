3da7d9b2561bf59977f70339f63204382232f5ad
Cali0707 <calumramurray@gmail.com>
2022-09-04 18:34:44 -0400

    Added delete transaction backend

--- server/src/controllers/ScuntTeamController.js
+++ server/src/controllers/ScuntTeamController.js
@@ -99,10 +99,21 @@ const ScuntTeamController = {
       next(e);
     }
   },
+
   async intializeTeams(req, res, next) {
     try {
       const createdTeams = await ScuntTeamServices.initializeTeams();
       res.status(200).send({ message: `Successfully created ${createdTeams.length} teams!` });
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  async deleteTransaction(req, res, next) {
+    try {
+      const { id, teamNumber } = req.body;
+      await ScuntTeamServices.deleteTransaction(teamNumber, id);
+      res.status(200).send({ message: 'Successfully deleted the transaction!' });
     } catch (e) {
       next(e);
     }
--- server/src/routes/scuntTeamRoutes.js
+++ server/src/routes/scuntTeamRoutes.js
@@ -166,6 +166,13 @@ router.post(
  */
 router.post('/transaction/check', checkLoggedIn, ScuntTeamController.checkTransaction);
 
+router.post(
+  'transaction/delete',
+  checkLoggedIn,
+  hasAuthScopes(['scunt:exec delete transaction']),
+  ScuntTeamController.deleteTransaction,
+);
+
 /**
  * @swagger
  * /scunt-teams/shuffle:
