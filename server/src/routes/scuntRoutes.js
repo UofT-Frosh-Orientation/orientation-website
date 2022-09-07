0e2af85efb8c1f019c3e23f6be98e5925d53e450
Cali0707 <calumramurray@gmail.com>
2022-09-07 11:10:14 -0400

    Fixed endpoints to match bot

--- server/src/routes/scuntRoutes.js
+++ server/src/routes/scuntRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.post('/login/discord', ScuntController.login);
 
-router.get('/mission/status', ScuntController.getMissionStatus);
+router.get('/get/mission/status', ScuntController.getMissionStatus);
 
-router.get('/leaderboard/scores', ScuntController.getLeaderboard);
+router.get('/get/leaderboard/scores', ScuntController.getLeaderboard);
 
 module.exports = router;
