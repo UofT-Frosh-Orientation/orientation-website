60b008ee0368b61bba067b8a41621ec3631895c4
James <50821962+jameskokoska@users.noreply.github.com>
2022-09-07 16:52:04 -0400

    ui fix

--- client/src/pages/ScuntMissionsList/ScuntMissionsList.jsx
+++ client/src/pages/ScuntMissionsList/ScuntMissionsList.jsx
@@ -55,7 +55,7 @@ const PageScuntMissionsList = () => {
         <Header text={'Missions'} underlineDesktop={'300px'} underlineMobile={'210px'}>
           <ScuntLinks />
           <div className="scunt-check-soon-title">
-            <h1>Check back soon!</h1>
+            <h1 style={{ color: 'var(--text-light)' }}>Check back soon!</h1>
           </div>
         </Header>
       </>
