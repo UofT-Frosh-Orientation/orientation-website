4b81ae026081a67db85f5962308487f44a32fb37
SherryZ <56203267+Sherryzh0830@users.noreply.github.com>
2023-06-02 23:11:44 +0800

    Updated Landing Page SZ
    Due to complications, I have modified the landing page and redesigned the styling.

--- client/src/pages/Initial/LandingPageSherry/LandingPageSherry.jsx
+++ client/src/pages/Initial/LandingPageSherry/LandingPageSherry.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from 'react';
-import PropTypes from 'prop-types';
 import './LandingPageSherry.scss';
 import InstagramLogo from '../../../assets/social/instagram_icon.png';
 
@@ -7,28 +6,26 @@ const SZLandingPage = () => {
   return (
     <>
       <section className="sz-wrapper-landing">
-        <div className="sz-landing-top">2023 Fall</div>
-        <div className="sz-landing-bottom" aria-hidden="true">
-          2023 Fall
-        </div>
-        <h1 className="szlanding-title-text">
-          F!rosh Week<br></br>is Approaching...
-        </h1>
+      <div className="sz-landing-text">
+        2023 Fall
+      </div>
+      <p className="szlanding-small-text">F!rosh Week is Approaching...</p>
         <div className="szlanding-logo-container">
-        <a href="https://www.instagram.com/froshweek/" target="_blank" rel="noreferrer">
-          <img
-            src={InstagramLogo}
-            className="sz-landing-instagram-logo"
-            alt="logo for F!rosh Instagram page"
-          />
-        </a>
-        <a href="https://www.instagram.com/froshnomore/" target="_blank" rel="noreferrer">
+        <h2 className="szlanding-bubble">Want to learn more?</h2>
+          <a href="https://www.instagram.com/froshweek/" target="_blank" rel="noreferrer">
+            <img
+              src={InstagramLogo}
+              className="sz-landing-instagram-logo"
+              alt="logo for F!rosh Instagram page"
+            />
+          </a><p>Check out our F!rosh Instagram page</p>
+          <a href="https://www.instagram.com/froshnomore/" target="_blank" rel="noreferrer">
             <img
               src={InstagramLogo}
               className="sz-landing-instagram-logo"
               alt="logo for leedur/skule upper years Instagram page"
             />
-          </a>
+          </a><p>Check out our FroshNoMore Instagram page</p>
         </div>
       </section>
     </>
