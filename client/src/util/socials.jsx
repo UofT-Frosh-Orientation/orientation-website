38ef312c6d48d0150356b8cf7fefbeea14f82579
Sherry <sherryzh0830@gmail.com>
2023-06-24 16:19:16 +0800

    Updated according to links in Linktree

--- client/src/util/socials.jsx
+++ client/src/util/socials.jsx
@@ -5,6 +5,7 @@ import SnapchatIcon from '../assets/social/snapchat-square-brands.svg';
 import YoutubeIcon from '../assets/social/youtube-square-brands.svg';
 import DiscordIcon from '../assets/social/discord-brands.svg';
 import TikTokIcon from '../assets/social/tiktok-brands.svg';
+import WhatsAppIcon from '../assets/social/square-whatsapp.svg';
 
 export const socials = [
   // {
@@ -17,15 +18,20 @@ export const socials = [
     link: 'https://bit.ly/froshig',
     icon: InstagramIcon,
   },
-  {
-    label: 'TikTok',
-    link: 'https://www.tiktok.com/@froshweek',
-    icon: TikTokIcon,
-  },
+  // {
+  //   label: 'TikTok',
+  //   link: 'https://www.tiktok.com/@froshweek',
+  //   icon: TikTokIcon,
+  // },
   {
     label: 'Discord',
-    link: 'https://discord.gg/NageuCrUk9',
+    link: 'https://discord.com/invite/6D9bBkU4ka',
     icon: DiscordIcon,
+  },
+  {
+    label: 'WhatsApp',
+    link: 'https://chat.whatsapp.com/L5ZKvpJIOD0JtiJDU9hj1U',
+    icon: WhatsAppIcon,
   },
   // {
   //   label: 'Twitter',
@@ -37,9 +43,9 @@ export const socials = [
   //   link: 'https://bit.ly/2N3dlXt',
   //   icon: SnapchatIcon,
   // },
-  // {
-  //   label: 'YoutubeIcon',
-  //   link: 'https://bit.ly/froshtube',
-  //   icon: YoutubeIcon,
-  // },
+  {
+    label: 'YoutubeIcon',
+    link: 'https://www.youtube.com/@skulefroshweek',
+    icon: YoutubeIcon,
+  },
 ];
