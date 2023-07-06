97fe74b7d4e44231a0c8d1f7df4881cac2a0d864
Tanushanth <79293445+Tanushanth@users.noreply.github.com>
2023-07-06 17:00:02 -0400

    Writing logs for usercontroller

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -4,7 +4,6 @@ const passport = require('../services/passport');
 const passwordResetSubscription = require('../subscribers/passwordResetSubscription');
 const announcementSubscription = require('../subscribers/announcementSubscription');
 const newUserSubscription = require('../subscribers/newUserSubscription');
-const {logger, loggerMiddleware} = require('../util/logger');
 
 const UserController = {
   /**
@@ -43,7 +42,7 @@ const UserController = {
       return res.status(200).send({ message: 'Success!', user: user.getResponseObject() });
     } 
     catch(err) {
-      req.log.error(err);
+      req.log.fatal({message: "Unable to create user"});
       next(err);
     }
   },
@@ -74,18 +73,18 @@ const UserController = {
   async login(req, res, next) {
     passport.authenticate('local', (err, user) => {
       if (err || !user) {
-        req.log.info("Inorrect Email and Password entered by user");
+        //req.log.info("Incorrect Email and Password entered by user");
         res.status(403).send({ message: 'Please ensure your email and password are correct.' });
       } else if (!user.confirmed) {
-        req.log.error(err, "Attempt to login with unverified email");
+       // req.log.error(err, "Attempt to login with unverified email");
         res.status(403).send({ message: 'Please ensure that you have verified your email.' });
       } else {
         req.logIn(user, (err) => {
           if (err) {
-            req.log.error(err);
+            req.log.fatal({message: "User Logout Failure: user " + user.id, err, user: user.getResponseObject()});
             next(err);
           } else {
-            req.log.info({message: "Successful login by user", user: user.getResponseObject()});
+            req.log.info({msg: "Successful login by user " + user.id , user: user.getResponseObject()});
             res.status(200).send({ message: 'Success!', user: user.getResponseObject() });
           }
         });
@@ -101,11 +100,14 @@ const UserController = {
    * @return {Promise<void>}
    */
   async logout(req, res, next) {
+    let user = req.user;
     req.logout((err) => {
       if (err) {
+        req.log.error({message: "User Logout Failure: user " + user.id, err, user: user.getResponseObject()});
         return next(err);
       } else {
-        return res.status(200).send({ message: 'Successfully logged out!' });
+        req.log.info({msg: "Successful Logout by user " + user.id, user: user.getResponseObject()});
+        return res.status(200).send({ message: "Successful Logout by user " + user.id, user: user.getResponseObject()});
       }
     });
   },
@@ -123,7 +125,7 @@ const UserController = {
           'If we have an account matching your email, we have sent an email to you. Please check there for instructions on how to reset your password',
       });
     } catch (err) {
-      console.log('Error in password reset request', err);
+      req.log.fatal({message: "User Password Reset Request Failure: user " + user.id, err, user: user.getResponseObject()});
       next(err);
     }
   },
@@ -143,6 +145,7 @@ const UserController = {
         });
       }
     } catch (err) {
+      req.log.fatal({message: "User Password Reset Request Failure: user " + user.id, err, user: user.getResponseObject()});
       next(err);
     }
   },
@@ -165,6 +168,7 @@ const UserController = {
         });
       }
     } catch (err) {
+      req.log.fatal({message: "Error with password reset page: user " + user.id, err, user: user.getResponseObject()});
       next(err);
     }
   },
@@ -179,6 +183,7 @@ const UserController = {
         .status(200)
         .send({ message: 'You have been successfully unsubscribed from announcement emails.' });
     } catch (error) {
+      req.log.error({message: "User Announcement Unsubscribe Error: user " + user.id, err, user: user.getResponseObject()});
       next(error);
     }
   },
@@ -189,8 +194,9 @@ const UserController = {
       await UserServices.resubscribeUser(email);
       res
         .status(200)
-        .send({ message: 'You have been successfully resubscribed from announcement emails.' });
+        .send({ message: 'You have been successfully resubscribed to announcement emails.' });
     } catch (error) {
+      req.log.error({message: "User Announcement Resubscribe Error: user " + user.id, err, user: user.getResponseObject()});
       next(error);
     }
   },
@@ -216,6 +222,7 @@ const UserController = {
           .send({ message: 'Successfully updated user!', user: updatedUser.getResponseObject() });
       }
     } catch (err) {
+      req.log.fatal({message: "User Request Auth Scope Error: user " + user.id, err, user: user.getResponseObject()});
       next(err);
     }
   },
@@ -228,6 +235,7 @@ const UserController = {
         unapprovedUsers: unapprovedUsers.map((u) => u.getResponseObject()),
       });
     } catch (err) {
+      req.log.error({message: "Error Getting Unapproved Users", err});
       next(err);
     }
   },
@@ -240,6 +248,7 @@ const UserController = {
         authRequests: usersAuthScopes.map((u) => u.getResponseObject()),
       });
     } catch (err) {
+      req.log.error({message: "Error Getting User Auth Scopes", err});
       next(err);
     }
   },
@@ -261,6 +270,7 @@ const UserController = {
         res.status(200).send({ message: 'Successfully approved users!' });
       }
     } catch (err) {
+      req.log.error({message: "Error updating account statuses", err});
       next(err);
     }
   },
@@ -271,6 +281,7 @@ const UserController = {
       await UserServices.updateAuthScopes(userAuthScopes);
       return res.status(200).send({ message: 'Auth scopes updated!' });
     } catch (err) {
+      req.log.error({message: "Error Updating Auth Scopes: user " + user.id, err, user: updatedUser.getResponseObject()});
       next(err);
     }
   },
@@ -283,6 +294,7 @@ const UserController = {
         authRequests: judgeUsers.map((u) => u.getResponseObject()),
       });
     } catch (e) {
+      req.log.error({message: "Error Getting Scunt Judge Users ", e});
       next(e);
     }
   },
@@ -300,6 +312,7 @@ const UserController = {
       await UserServices.deleteUser(id);
       res.status(200).send({ message: 'Successfully deleted User!', deletedId: id });
     } catch (err) {
+      req.log.fatal({message: "Error Deleting User " + id, err});
       next(err);
     }
   },
@@ -326,7 +339,7 @@ const UserController = {
         });
       }
     } catch (e) {
-      console.log(e);
+      req.log.error({message: "Error Determining if User Exists", e});
       next(e);
     }
   },
--- server/src/util/logger.js
+++ server/src/util/logger.js
@@ -3,38 +3,32 @@ const pinoHttp = require('pino-http');
 const pinoPretty = require('pino-pretty');
 
 
-const fileStream = pino.destination('./output.log');
-
+const fileStream = pino.destination({dest: './output.log', sync: true});
 const logger = pino({
     transport:{
-        target: "pino-pretty",
-        options:{
-            translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
-        }
+        targets: [
+            {
+                target: "pino-pretty",
+                options:{
+                    translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
+                }
+            },
+        ]
     },
     redact: {
-        paths: ['req.headers'],
+        paths: ['req.headers', 'req.remoteAddress', 'req.remotePort', 'res.headers', 'user.email', 'user.firstName', 'user.lastName', 'user.preferredName'],
         remove: true
       }
   }, fileStream);
 
 const loggerMiddleware = pinoHttp({
     logger,
-    customLogLevel: function (res, err) {
-        // Customize the log level based on the response and error
-        return 'info';
-    },
-    customSuccessMessage: function (res) {
+    customSuccessMessage: function (req, res) {
         // Customize the success log message
-        return `Request completed successfully with status ${res.statusCode}`;
-    },
-    customErrorMessage: function (error, res) {
-        // Customize the error log message
-        return `An error occurred: ${error.message}`;
+        return "Request completed successfully with status " + res.statusCode;
     },
     prettyPrint: { colorize: true }, // Enable pretty printing with colors
-    }
-);
+    });
 
 module.exports = {
     logger,
