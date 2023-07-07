13ce5de9b9c548d05b606886bd80f51a7518e5c0
Farbod M <farbodm.dev@gmail.com>
2023-07-07 19:12:43 -0400

    quick fixes

--- server/src/util/logger.js
+++ server/src/util/logger.js
@@ -1,7 +1,8 @@
 const pino = require('pino');
 const pinoHttp = require('pino-http');
 
-const logger = pino({
+const logger = pino(
+  {
     redact: {
       paths: [
         'req.headers',
@@ -16,12 +17,12 @@ const logger = pino({
       remove: true,
     },
   },
-  pino.destination({ dest: `${__dirname}/../output.txt`, minLength: 2048, sync: false }),
+  pino.destination({ dest: `${__dirname}/../backendlogs.txt`, minLength: 4096, sync: false }),
 );
 
 const loggerMiddleware = pinoHttp({
   logger,
-  customSuccessMessage: function (req, res) {
+  customSuccessMessage(req, res) {
     // Customize the success log message
     return 'Request completed successfully with status ' + res.statusCode;
   },
