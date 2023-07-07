8f79f98b6ed8224fb821ad18a9cd605be6c99e69
Tanushanth <79293445+Tanushanth@users.noreply.github.com>
2023-07-07 15:12:28 -0400

    Updated logger file to fix output file not writing (Thnx Farbod)

--- server/src/util/logger.js
+++ server/src/util/logger.js
@@ -1,35 +1,36 @@
 const pino = require('pino');
 const pinoHttp = require('pino-http');
 
-const fileStream = pino.destination({ dest: './output.log', minLength: 512, sync: false });
-
-const logger = pino({
-    transport:{
-        targets: [
-            {
-                target: "pino-pretty",
-                options:{
-                    translateTime: "SYS:dd-mm-yyyy HH:MM:ss",
-                }
-            },
-        ]
+const logger = pino(
+  [
+    {
+      redact: {
+        paths: [
+          'req.headers',
+          'req.remoteAddress',
+          'req.remotePort',
+          'res.headers',
+          'user.email',
+          'user.firstName',
+          'user.lastName',
+          'user.preferredName',
+        ],
+        remove: true,
+      },
     },
-    redact: {
-        paths: ['req.headers', 'req.remoteAddress', 'req.remotePort', 'res.headers', 'user.email', 'user.firstName', 'user.lastName', 'user.preferredName'],
-        remove: true
-      }
-}, fileStream);
+  ],
+  pino.destination({ dest: `${__dirname}/../output.txt`, minLength: 2048, sync: false }),
+);
 
 const loggerMiddleware = pinoHttp({
-    logger,
-    customSuccessMessage: function (req, res) {
-        // Customize the success log message
-        return "Request completed successfully with status " + res.statusCode;
-    },
-    prettyPrint: { colorize: true }, // Enable pretty printing with colors
+  logger,
+  customSuccessMessage: function (req, res) {
+    // Customize the success log message
+    return 'Request completed successfully with status ' + res.statusCode;
+  },
 });
 
 module.exports = {
-    logger,
-    loggerMiddleware
-}
+  logger,
+  loggerMiddleware,
+};
