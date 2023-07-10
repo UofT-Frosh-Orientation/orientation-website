4fc346f8fea86b63e7648c0996368a91200672d8
Natalie Chan <natapoke2@gmail.com>
2023-07-09 21:16:27 -0400

    adding bg

--- client/src/pages/404/404.jsx
+++ client/src/pages/404/404.jsx
@@ -1,38 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import DinoBones from '../../assets/404/dino-bones-new.svg';
-import Grass from '../../assets/404/grass.png';
-import Meteor from '../../assets/404/meteor.png';
 import './404.scss';
-
 import background from '../../assets/404/bg.png';
 import cat from '../../assets/404/cat.svg';
 import errorMsg from '../../assets/404/ERROR.svg';
 import four from '../../assets/404/four.svg';
 import sign from '../../assets/404/sign.svg';
 import tree from '../../assets/404/tree.svg';
-
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 
 const Page404 = () => {
   return (
     <>
-      <div className="error404-container">
-        <div className="error404-flex">
-          <img src={four} className="error404-four error404-right"></img>
+      <LazyLoadComponent>
+        <div className="error404-container">
+          <img src={background} className="error404-bg"></img>
+          <div className="error404-flex">
+            <img src={four} className="error404-four error404-right"></img>
+          </div>
+          <div className="error404-flex">
+            <img src={cat} className="error404-cat"></img>
+          </div>
+          <div className="error404-flex">
+            <img src={four} className="error404-four error404-left"></img>
+          </div>
+          <img src={sign} className="error404-sign"></img>
+          <img src={tree} className="error404-tree"></img>
+          <div className="error404-flex">
+            <img src={errorMsg} className="error404-msg"></img>
+          </div>
         </div>
-        <div className="error404-flex">
-          <img src={cat} className="error404-cat"></img>
-        </div>
-        <div className="error404-flex">
-          <img src={four} className="error404-four error404-left"></img>
-        </div>
-        <img src={sign} className="error404-sign"></img>
-        <img src={tree} className="error404-tree"></img>
-        <div className="error404-flex">
-          <img src={errorMsg} className="error404-msg"></img>
-        </div>
-      </div>
+      </LazyLoadComponent>
     </>
   );
 };
