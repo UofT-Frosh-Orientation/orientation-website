2dc79c839b3d5e1b7d9d2cf558c16975296a74e1
Freeassassin <farbodemzadeh@gmail.com>
2023-08-14 15:15:51 -0400

    Update chunk settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import { dependencies } from './package.json';
+
+function renderChunks(deps) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom', '@react-pdf/renderer'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +24,13 @@ export default defineConfig({
   },
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
   },
 });
