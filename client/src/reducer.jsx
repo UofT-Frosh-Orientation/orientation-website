da49f84def9dde7e81572dcdc737db1637f0912d
Farbod M <farbodm.dev@gmail.com>
2023-08-19 17:56:51 -0400

    comment out scunt

--- client/src/reducer.jsx
+++ client/src/reducer.jsx
@@ -3,15 +3,15 @@ import userReducer from './state/user/userSlice';
 import accountsReducer from './state/accounts/accountSlice';
 import announcementsReducer from './state/announcements/announcementsSlice';
 import froshReducer from './state/frosh/froshSlice';
-import scuntSettingsReducer from './state/scuntSettings/scuntSettingsSlice';
-import scuntMissionsReducer from './state/scuntMissions/scuntMissionsSlice';
+// import scuntSettingsReducer from './state/scuntSettings/scuntSettingsSlice';
+// import scuntMissionsReducer from './state/scuntMissions/scuntMissionsSlice';
 
 const rootReducer = combineReducers({
   userReducer,
   accountsReducer,
   froshReducer,
-  scuntSettingsReducer,
-  scuntMissionsReducer,
+  // scuntSettingsReducer,
+  // scuntMissionsReducer,
   announcementsReducer,
 });
 
