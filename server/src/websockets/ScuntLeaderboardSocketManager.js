1fde45f5fec9e593208a0e57d2e66b1ae5d52075
Freeassassin <farbodemzadeh@gmail.com>
2023-08-24 17:12:14 -0400

    update scunt websocket

--- server/src/websockets/ScuntLeaderboardSocketManager.js
+++ server/src/websockets/ScuntLeaderboardSocketManager.js
@@ -33,8 +33,8 @@ class ScuntLeaderboardSocketManager {
 
   async getScores(socket) {
     console.log('Getting scores!');
-    console.log(this.settings.revealLeaderboard);
-    if (this.settings.revealLeaderboard) {
+    console.log(this.settings?.revealLeaderboard);
+    if (this.settings?.revealLeaderboard) {
       const scores = await ScuntTeamServices.getTeamPoints();
       socket.emit('scores', scores);
     }
@@ -47,7 +47,7 @@ class ScuntLeaderboardSocketManager {
         data: { team, score },
       } = job;
       const lastUpdated = this.teamUpdateTimes.get(team) ?? 0;
-      if (timestamp >= lastUpdated && this.settings.revealLeaderboard) {
+      if (timestamp >= lastUpdated && this.settings?.revealLeaderboard) {
         this.sendUpdate(team, score);
         this.teamUpdateTimes.set(team, timestamp);
       }
@@ -63,7 +63,7 @@ class ScuntLeaderboardSocketManager {
         const newSettings = job.data;
         if (!newSettings) {
           return done();
-        } else if (newSettings.revealLeaderboard) {
+        } else if (newSettings?.revealLeaderboard) {
           console.log('Revealing leaderboard!');
           ScuntTeamServices.getTeamPoints().then((scores) => {
             console.log('team scores');
