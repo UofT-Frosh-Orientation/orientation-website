cf21c2cb19284fe8729b01f02b6d89f0468ec356
Freeassassin <farbodemzadeh@gmail.com>
2023-08-24 17:11:41 -0400

    add scunt teams state

--- client/src/reducer.jsx
+++ client/src/reducer.jsx
@@ -5,6 +5,7 @@ import announcementsReducer from './state/announcements/announcementsSlice';
 import froshReducer from './state/frosh/froshSlice';
 import scuntSettingsReducer from './state/scuntSettings/scuntSettingsSlice';
 import scuntMissionsReducer from './state/scuntMissions/scuntMissionsSlice';
+import scuntTeamsReducer from './state/scuntTeams/scuntTeamsSlice';
 
 const rootReducer = combineReducers({
   userReducer,
@@ -13,6 +14,7 @@ const rootReducer = combineReducers({
   scuntSettingsReducer,
   scuntMissionsReducer,
   announcementsReducer,
+  scuntTeamsReducer,
 });
 
 export default rootReducer;
