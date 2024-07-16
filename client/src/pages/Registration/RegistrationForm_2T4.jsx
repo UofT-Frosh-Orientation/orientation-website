88dacaefa5c2022187be36bbf1416dcf2a48bbdb
Jiahan William Wen <wenjiahan2002@gmail.com>
2024-07-16 19:49:41 -0400

    Copy the old RegistrtionForm to RegistrationForm_2T4

--- client/src/pages/Registration/RegistrationForm_2T4.jsx
+++ client/src/pages/Registration/RegistrationForm_2T4.jsx
@@ -18,7 +18,6 @@ import { useSelector } from 'react-redux';
 import { ErrorSuccessBox } from '../../components/containers/ErrorSuccessBox/ErrorSuccessBox';
 
 const PageRegistrationForm = ({ editFieldsPage, initialValues, onEditSubmit }) => {
-  console.log('This is a new version of Registration Form since 2T4 uu aa');
   const steps = Object.keys(fields);
   const [froshObject, setFroshObject] = useState({});
   const [formFields, setFormFields] = useState(fields);
@@ -39,7 +38,7 @@ const PageRegistrationForm = ({ editFieldsPage, initialValues, onEditSubmit }) =
     if (registered && !editFieldsPage) {
       navigate('/profile');
     }
-  }, [registered, editFieldsPage, navigate]);
+  }, []);
 
   const handleRegister = async () => {
     setCanRegister(false);
@@ -85,7 +84,6 @@ const PageRegistrationForm = ({ editFieldsPage, initialValues, onEditSubmit }) =
         //   console.log(`${key}: ${value}`);
         // }
         froshObject['id'] = user.id;
-        console.log(user.id);
         const ReactPDF = await import('@react-pdf/renderer');
         const { MakeReceipt } = await import('../../components/MakeReceipt/MakeReceipt');
         const dataReceipt = await ReactPDF.pdf(MakeReceipt(froshObject)).toBlob();
@@ -121,10 +119,11 @@ const PageRegistrationForm = ({ editFieldsPage, initialValues, onEditSubmit }) =
   useEffect(() => {
     for (let step of steps) {
       Object.keys(formFields[step]).map((key, index) => {
+        // The form components will run onChange at mount if given a default value (so it will already be populated, we don't want to overwrite this value)
         if (froshObject[key] === undefined) return (froshObject[key] = undefined);
       });
     }
-  }, [formFields, froshObject, steps]);
+  }, []);
 
   const validateForm = () => {
     let validated = true;
@@ -200,10 +199,7 @@ const PageRegistrationForm = ({ editFieldsPage, initialValues, onEditSubmit }) =
                   localStorageKey={editFieldsPage === true ? undefined : field.localStorageKey}
                   placeholder={field.placeholder}
                   onChange={(value) => {
-                    setFroshObject((prevState) => ({
-                      ...prevState,
-                      [key]: value,
-                    }));
+                    froshObject[key] = value;
                     if (field.onChanged) field.onChanged(value, disableField);
                   }}
                   isPhoneNumber={field.isPhoneNumber}
