647572a178e4b46aa3e0e38b7614e1daa84e3e66
Alyssa Hee <alyssahee43@gmail.com>
2025-06-29 17:53:01 -0500

    Changed pressed-in button highlight and shadow

--- client/src/util/colors.jsx
+++ client/src/util/colors.jsx
@@ -112,8 +112,8 @@ export const colors = {
   '--button-outlined-font': ['#20002e', '#ffffff'],
   '--button-outlined-disabled-border': ['#989898', '#8c60b3'],
   '--button-disabled': ['#BBBBBB', '#300150'],
-  '--button-pressed-highlight': ['#cdb4e0', '#795696'],
-  '--button-pressed-shadow': ['#000000a0', '#000000a0'], // Used in 2T5
+  '--button-pressed-highlight': ['#ffffff36', '#ffffff36'],
+  '--button-pressed-shadow': ['#00000052', '#00000052'], // Used in 2T5
   '--button-active-bg': ['#DFCFE6', '#7b539e'],
 
   //about us teams tab
