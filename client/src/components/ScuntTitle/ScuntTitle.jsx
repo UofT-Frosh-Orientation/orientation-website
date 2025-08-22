073e38738bcbf6e39e70d62f731b743f8016b3bf
Ablah <115513366+Ablahgad@users.noreply.github.com>
2025-08-21 23:31:29 -0600

    Added countdown underneath scunt title

--- client/src/components/ScuntTitle/ScuntTitle.jsx
+++ client/src/components/ScuntTitle/ScuntTitle.jsx
@@ -1,9 +1,94 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { userSelector } from '../../state/user/userSlice';
+import { scuntSettingsSelector } from '../../state/scuntSettings/scuntSettingsSlice';
+import useAxios from '../../hooks/useAxios';
+import { scuntTeamsSelector } from '../../state/scuntTeams/scuntTeamsSlice';
+import { getScuntSettings } from '../../state/scuntSettings/saga';
+import { getScuntTeams } from '../../state/scuntTeams/saga';
+const { axios } = useAxios();
+
+
 export const ScuntTitle = () => {
   return(
     <div className = 'scunt-title-container'>
       <img className = 'scunt-title-image' src='\src\assets\scunt\scunt-photo.jpg'></img>
       <div className='scunt-title-gradient'></div>
       <h1 className='scunt-home-title-text'>SKULE<br />HUNT</h1>
+      <ScuntCountdown />
     </div>
   );
 ;}
+
+const ScuntCountdown = () => {
+  const { scuntSettings, loading } = useSelector(scuntSettingsSelector);
+  const [targetDate, setTargetDate] = useState();
+  const [countDownDate, setCountDownDate] = useState();
+  const [countDown, setCountDown] = useState(countDownDate - new Date().getTime());
+
+  useEffect(() => {
+    if (scuntSettings !== undefined) {
+      let settings = scuntSettings;
+      // const tempDate = new Date(settings?.scuntDate);
+      const tempDate = new Date('2025-08-28T18:00:00');
+      const tempCountDownDate = new Date(tempDate).getTime();
+
+      setTargetDate(tempDate);
+      setCountDownDate(tempCountDownDate);
+    }
+  }, [scuntSettings]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(countDownDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+
+  const getDateValues = (countDown) => {
+    if (countDown <= 0) {
+      return [0, 0, 0, 0];
+    }
+    const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+    return [days, hours, minutes, seconds];
+  };
+
+  const checkNaN = (value) => {
+    if (isNaN(value)) {
+      return 0;
+    } else {
+      return value;
+    }
+  };
+
+  return (
+    <div className="scunt-countdown-wrap">
+      <div className="scunt-countdown">
+        <div className="scunt-countdown-number">
+          <h1>{checkNaN(getDateValues(countDown)[0])}</h1>
+          <h3>DAYS</h3>
+        </div>
+        <div className="scunt-countdown-number">
+          <h1>{checkNaN(getDateValues(countDown)[1])}</h1>
+          <h3>HOURS</h3>
+        </div>
+        <div className="scunt-countdown-number">
+          <h1>{checkNaN(getDateValues(countDown)[2])}</h1>
+          <h3>MINUTES</h3>
+        </div>
+        <div className="scunt-countdown-number">
+          <h1>{checkNaN(getDateValues(countDown)[3])}</h1>
+          <h3>SECONDS</h3>
+        </div>
+      </div>
+      {/* Only show confetti for the first 100 seconds overtime */}
+      {countDown <= 0 && countDown / 1000 >= -100 ? <Confetti animate={true} /> : <></>}
+    </div>
+  );
+};
