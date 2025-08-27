5aa9a02aa2e4278f763c36a3c2bffa79335585a0
Alyssa Hee <alyssahee43@gmail.com>
2025-08-27 17:14:45 -0400

    Fix title position

--- client/src/components/ScuntTitle/ScuntTitle.jsx
+++ client/src/components/ScuntTitle/ScuntTitle.jsx
@@ -12,8 +12,7 @@ import './ScuntTitle.scss';
 
 export const ScuntTitle = () => {
   return (
-    //<div className="scunt-title-container">
-    <>
+    <div className="scunt-title-container">
       <img className="scunt-title-image" src="\src\assets\scunt\scunt-photo.jpg"></img>
       <div className="scunt-title-gradient"></div>
       <h1 className="scunt-home-title-text">
@@ -22,8 +21,7 @@ export const ScuntTitle = () => {
         HUNT
       </h1>
       <ScuntCountdown />
-    </>
-    //</div>
+    </div>
   );
 };
 
