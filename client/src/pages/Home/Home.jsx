558b3652c5cb8da2e8f5d75f29715cc725d1a1f5
Ablah <115513366+Ablahgad@users.noreply.github.com>
2026-06-09 11:47:42 -0700

    Turned on registration

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -83,7 +83,7 @@ const HomePageHeader = () => {
 
 const HomeHeaderButton = () => {
   const loggedIn = useSelector(loggedInSelector);
-  const [isRegistrationOpen, setIsRegistrationOpen] = useState(false);
+  const [isRegistrationOpen, setIsRegistrationOpen] = useState(true);
 
   return (
     <>
