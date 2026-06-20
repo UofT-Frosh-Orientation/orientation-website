16eab4394dc9407b9a03e2af985ef75494995cfc
SRuhil <120292312+SRuhil@users.noreply.github.com>
2026-06-20 20:54:06 +0530

    Animated Dance Floor Animation Added

--- client/src/pages/Schedule/Schedule.jsx
+++ client/src/pages/Schedule/Schedule.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Schedule.scss';
 
 export const PageSchedule = () => {
+  // 150 tiles ensures coverage for screens up to 3000px wide (30 columns * 5 rows)  const totalTiles = 50;
+  const totalTiles = 150;
+  const danceFloorTiles = Array.from({ length: totalTiles });
+
   return (
     <section className="schedule-hero">
       <div className="hero-background">
         <div className="checkerboard" />
-        <div className="dance-floor" />
+
+        <div className="dance-floor">
+          {danceFloorTiles.map((_, index) => (
+            <div key={index} className="dance-tile" />
+          ))}
+        </div>
       </div>
 
       <div className="hero-foreground">
