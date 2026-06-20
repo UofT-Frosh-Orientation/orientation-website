048ecba36861c543be35c1af3fd26e5ac184c5b8
SRuhil <120292312+SRuhil@users.noreply.github.com>
2026-06-20 16:45:58 +0530

    Added Purple Checker Pattern
    Purple checker spans half the viewport, aspect ratio fixed to ensure checkers are always square

--- client/src/pages/Schedule/Schedule.jsx
+++ client/src/pages/Schedule/Schedule.jsx
@@ -1,8 +1,20 @@
+import React from 'react';
+import './Schedule.scss';
+
 export const PageSchedule = () => {
   return (
-    <div>
-      <h1>Schedule</h1>
-      <p>This is the schedule page.</p>
-    </div>
+    <section className="schedule-hero">
+      <div className="hero-background">
+        <div className="checkerboard" />
+        <div className="dance-floor" />
+      </div>
+
+      <div className="hero-foreground">
+        <div className="schedule-ellipse">
+          <h1>Schedule</h1>
+          <p>September 1 - 4</p>
+        </div>
+      </div>
+    </section>
   );
 };
