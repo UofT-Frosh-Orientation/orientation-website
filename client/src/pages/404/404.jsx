4f8a94947fdcfe3a369a7ebf6b93ba2693cbf155
ParthB21 <parthbhasin21@gmail.com>
2026-06-26 01:39:21 -0400

    added 404

--- client/src/pages/404/404.jsx
+++ client/src/pages/404/404.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './404.scss';
-import error404 from '../../assets/404/ERROR404.png';
-import { LazyLoadComponent, LazyLoadImage } from 'react-lazy-load-image-component';
+import sadLogo from '../../assets/404/sadlogo404.svg';
 
 const Page404 = () => {
-  console.log('404 page loaded');
   return (
-    <>
-      <LazyLoadImage className="error-background" src={error404} alt="error404" />
-    </>
+    <div className="error-404">
+      <img className="error-404-logo" src={sadLogo} alt="Page not found" />
+      <h1 className="error-404-title">Error 404</h1>
+      <p className="error-404-subtitle">Page not found</p>
+    </div>
   );
 };
 
