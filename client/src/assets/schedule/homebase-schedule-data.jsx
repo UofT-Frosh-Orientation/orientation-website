feaf468dc1d3687563c3b4e12da44e406debd04d
SRuhil <120292312+SRuhil@users.noreply.github.com>
2026-08-14 19:00:07 -0400

    Added a placeholder Homebase schedule
    Same format as the current schedule data set

---
+++ client/src/assets/schedule/homebase-schedule-data.jsx
@@ -0,0 +1,114 @@
+export const data = {
+  'MONDAY AUGUST 31': [
+    {
+      'Event Name': 'Temporary Drive in Schedule',
+      // TODO: add description
+      'Start Time': '8:00 AM',
+      'End Time': '9:00 AM',
+      Color: 'purple',
+    },
+    {
+      'Event Name': 'Matriculation',
+      'Event Description':
+        'Our kickoff to F!rosh Week and introduction to all things Skule™. We all gather in Convocation Hall to meet the wonderful UofT Engineering community!',
+      'Start Time': '9:00 AM',
+      'End Time': '11:00 AM',
+      Color: 'yellow',
+    },
+    {
+      'Event Name': 'Lunch',
+      // TODO: add description
+      'Start Time': '11:00 AM',
+      'End Time': '12:00 PM',
+      Color: 'green',
+    },
+    {
+      'Event Name': 'D!ye/F!rosh Games',
+      'Event Description':
+        'D!ye: When I say purple, you say purple! Purple! PURPLE! D!ye is a 40+ year tradition where F!rosh and Leedurs dye their bodies (or any part thereof) purple. The color purple has a long-standing history across Engineering schools in Canada, and allows us to honour the engineers that came before us. <br /> F!rosh Games: Compete against other F!rosh Groups in the craziest competitions imaginable!',
+      'Start Time': '12:00 PM',
+      'End Time': '3:00 PM',
+      Color: 'yellow',
+    },
+    {
+      'Event Name': 'Downtown Walkaround',
+      'Event Description':
+        'Join us for a lovely tour of Toronto’s downtown with 1000 of your newest friends and classmates',
+      'Start Time': '3:00 PM',
+      'End Time': '5:00 PM',
+      Color: 'purple',
+    },
+    {
+      'Event Name': 'Break/Dinner',
+      // TODO: add description
+      'Start Time': '5:00 PM',
+      'End Time': '6:00 PM',
+      Color: 'green',
+    },
+    {
+      'Event Name': 'Nitelife - NeonNight: Glow BIG or GoHome',
+      'Event Description': 'Slime making and Board Games!!',
+      'Event Location': 'Chem Common room, WB238',
+      'Start Time': '6:00 PM',
+      'End Time': '9:00 PM',
+
+      Color: 'dark-purple',
+    },
+  ],
+  'TUESDAY SEPTEMBER 1': [
+    {
+      'Event Name': 'Temporary Drive in Schedule',
+      'Event Description':
+        'Learn about the strategies you need to know to succeed here at U of T Engineering from some of your amazing new TAs (teaching assistants)!',
+      'Start Time': '8:00 AM',
+      'End Time': '10:00 AM',
+      Color: 'gray',
+    },
+    {
+      'Event Name': 'Campus Tours/Build Battle',
+      'Event Description':
+        'Campus Tours: Join your F!rosh Leedurs as they lead you through a tour of campus to find all the best places to study, eat, work on projects, or even nap! <br /> Build Battle: Your first of many design challenges at UofT Engineering, except it’s probably the one of the coolest ones you’ll do! Work with your F!rosh Group to tackle your piece of the puzzle for this year’s unique design challenge, coming together to build something truly amazing as a 3T0 class!',
+      'Start Time': '10:00 AM',
+      'End Time': '12:00 PM',
+      Color: 'yellow',
+    },
+    {
+      'Event Name': 'Lunch',
+      // TODO: add description
+      'Start Time': '12:00 PM',
+      'End Time': '1:00 PM',
+      Color: 'green',
+    },
+    {
+      'Event Name': 'Campus Tours/Build Battle',
+      'Event Description':
+        "Campus Tours: Join your F!rosh Leedurs as they lead you through a tour of campus to find all the best places to study, eat, work on projects, or even nap! <br /> Build Battle: Your first of many design challenges at UofT Engineering, except it's probably the one of the coolest ones you’ll do! Work with your F!rosh Group to tackle your piece of the puzzle for this year's unique design challenge, coming together to build something truly amazing as a 3T0 class!",
+      'Start Time': '1:00 PM',
+      'End Time': '5:00 PM',
+      Color: 'yellow',
+    },
+    {
+      'Event Name': 'Cheer Off',
+      // TODO: add description
+      'Start Time': '5:00 PM',
+      'End Time': '6:00 PM',
+      Color: 'yellow',
+    },
+    {
+      'Event Name': 'Break/Dinner',
+      // TODO: add description
+      'Start Time': '6:00 PM',
+      'End Time': '7:00 PM',
+      Color: 'green',
+    },
+    {
+      'Event Name': 'Nitelife - Theater Theatrics: HollywoodHideout',
+      // TODO: add description (and location if applicable)
+      'Event Description': 'Hide and Seek Game + Movie Night!!',
+      'Event Location': 'Myhal 5th Floor',
+      'Start Time': '7:00 PM',
+      'End Time': 'Late',
+      Color: 'dark-purple',
+    },
+  ],
+};
