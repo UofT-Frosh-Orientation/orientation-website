58dc61f2cfbeb347c182c364e994df260bf968c8
Ablah <115513366+Ablahgad@users.noreply.github.com>
2026-08-18 13:01:25 -0600

    BIPOC other event added

--- client/src/pages/Home/otherevents.jsx
+++ client/src/pages/Home/otherevents.jsx
@@ -1,27 +1,36 @@
 import bso from '../../assets/OtherEvents/bso.jpg';
 import campus from '../../assets/OtherEvents/campus.webp';
 import spadina from '../../assets/OtherEvents/koffler.jpg';
+import bipoc from '../../assets/OtherEvents/bipoc.jpg'
 
 export const otherEventsData = [
   {
-    title: 'Black Student Orientation',
+    title: 'BIPOC Resource Fair',
     description:
-      'Want to celebrate Black culture, embrace the community at the University of Toronto, and prepare yourself for the UofT experience? Black Student Orientation, organized by Black Students for Black students, is an opportunity to make a smooth transition into university life, make new friends, and start the year on a positive note! Registration for this FREE orientation is open!',
-    image: bso,
-    link: 'https://www.instagram.com/uoftbso/',
-  },
-  {
-    title: 'Faculty Orientation Events',
-    description:
-      'Check out the schedule for Faculty Orientation Events happening on Thursday, September 3rd!',
-    image: campus,
-    link: 'https://undergrad.engineering.utoronto.ca/event/faculty-orientation-events-2/',
-  },
-  {
-    title: 'Spadina Block Party',
-    description:
-      "Join us for the Multi-Faith Centre Spadina Block Party, a welcoming and inclusive event for all U of T students. Whether you're a first-year, a returning student, or just curious about what the Multi-Faith Centre offers, this event is a chance to meet new people, explore resources, and celebrate our diverse campus community. Connect with student life units and U of T divisions that support well-being, spirituality, and community building.",
-    image: spadina,
-    link: 'https://folio.utoronto.ca/students/events/detail/5645789',
-  },
+      'Join us for fun, food, music, and student organizations and university services centring BIPOC students! New or returning students from all colleges are welcome! ',
+    image: bipoc,
+    link: 'https://www.instagram.com/lifeatnew/',
+  }
+  // ,
+  // {
+  //   title: 'Black Student Orientation',
+  //   description:
+  //     'Want to celebrate Black culture, embrace the community at the University of Toronto, and prepare yourself for the UofT experience? Black Student Orientation, organized by Black Students for Black students, is an opportunity to make a smooth transition into university life, make new friends, and start the year on a positive note! Registration for this FREE orientation is open!',
+  //   image: bso,
+  //   link: 'https://www.instagram.com/uoftbso/',
+  // },
+  // {
+  //   title: 'Faculty Orientation Events',
+  //   description:
+  //     'Check out the schedule for Faculty Orientation Events happening on Thursday, September 3rd!',
+  //   image: campus,
+  //   link: 'https://undergrad.engineering.utoronto.ca/event/faculty-orientation-events-2/',
+  // },
+  // {
+  //   title: 'Spadina Block Party',
+  //   description:
+  //     "Join us for the Multi-Faith Centre Spadina Block Party, a welcoming and inclusive event for all U of T students. Whether you're a first-year, a returning student, or just curious about what the Multi-Faith Centre offers, this event is a chance to meet new people, explore resources, and celebrate our diverse campus community. Connect with student life units and U of T divisions that support well-being, spirituality, and community building.",
+  //   image: spadina,
+  //   link: 'https://folio.utoronto.ca/students/events/detail/5645789',
+  // },
 ];
